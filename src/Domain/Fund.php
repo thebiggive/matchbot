7e0259d0df31360087a16eef66008b251e9b3555
Noel Light-Hilary <noel@noellh.com>
2019-10-29 09:55:10 +0000

    MAT-5 - use single table inheritance for Fund

--- src/Domain/Fund.php
+++ src/Domain/Fund.php
@@ -8,19 +8,15 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
+ * @ORM\InheritanceType("SINGLE_TABLE")
+ * @ORM\DiscriminatorColumn(name="fundType", type="string")
+ * @ORM\DiscriminatorMap({"championFund" = "ChampionFund", "pledge" = "Pledge", "unknownFund" = "Fund"})
  * @ORM\HasLifecycleCallbacks
  * @ORM\Table
  */
-class Fund extends SalesforceProxy
+abstract class Fund extends SalesforceProxy
 {
     use TimestampsTrait;
-
-    /**
-     * @ORM\Column(type="string", length=8)
-     * @var string  'champion' or 'pledger'. Avoiding enum because of drawbacks noted at
-     *              @link https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/cookbook/mysql-enums.html
-     */
-    protected $fundType;
 
     /**
      * @ORM\Column(type="decimal", precision=18, scale=2)
