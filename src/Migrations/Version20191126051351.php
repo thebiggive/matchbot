3aa43a45b18c342d3c475b25766a50d1f809acf8
Noel Light-Hilary <noel@noellh.com>
2019-11-26 05:21:14 +0000

    MAT-33 - remove schema-level tip default

---
+++ src/Migrations/Version20191126051351.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Remove buggy default tip value
+ */
+final class Version20191126051351 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Remove buggy default tip value';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Donation CHANGE tipAmount tipAmount NUMERIC(18, 2) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Donation CHANGE tipAmount tipAmount NUMERIC(18, 2) DEFAULT \'0.00\' NOT NULL');
+    }
+}
