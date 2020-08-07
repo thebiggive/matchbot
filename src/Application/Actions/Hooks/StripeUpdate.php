84a6be901b6d010ae962951f1dd9207972379d9f
tbg-jasonfung <jason@thebiggive.org.uk>
2020-08-07 11:05:47 +0100

    MAT-85 begin handling payment intent succeeded events

--- src/Application/Actions/Hooks/StripeUpdate.php
+++ src/Application/Actions/Hooks/StripeUpdate.php
@@ -16,6 +16,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Log\LoggerInterface;
 use Slim\Exception\HttpBadRequestException;
 use Symfony\Component\Serializer\SerializerInterface;
+use Stripe\Event as StripeEvent;
 
 class StripeUpdate extends Action
 {
@@ -56,14 +57,77 @@ class StripeUpdate extends Action
             return $this->respond(new ActionPayload(400, null, $error));
         }
 
-        $payload = new ActionPayload(200, $event->data->object);
+        // $payload = new ActionPayload(200, $event->data->object);
 
+        // Handle the event
+        switch ($event->type) {
+            case 'payment_intent.succeeded':
+                $this->handlePaymentIntentSucceeded($event);
+                break;
+            case 'payment_intent.created':
+                $logMessage = 'Unsupported Action';
+                $this->logger->warning($logMessage);
+                $error = new ActionError(ActionError::BAD_REQUEST, null ?? $logMessage);
+                return $this->respond(new ActionPayload(400, null, $error));
+            default:
+                $logMessage = 'Unsupported Action';
+                $this->logger->warning($logMessage);
+                $error = new ActionError(ActionError::BAD_REQUEST, null ?? $logMessage);
+                return $this->respond(new ActionPayload(400, null, $error));
+            }
+
+        return $this->respondWithData($event->data->object);
+    }
+
+    public function handlePaymentIntentSucceeded(StripeEvent $event): Response
+    {
         /** @var Donation $donation */
         $donation = $this->donationRepository->findOneBy(['transactionId' => $event->data->object->id]);
 
         if (!$donation) {
             throw new DomainRecordNotFoundException('Donation not found');
         }
+
+        $missingRequiredField = (
+            empty($event->status) ||
+            empty($event->data->object->billing_details->address->postal_code) ||
+            empty($event->data->object->billing_details->address->country) ||
+            empty($event->data->object->billing_details->email) ||
+            empty($event->data->object->billing_details->name) ||
+            !isset($event->data->metadata->coreDonationGiftAid, $event->data->metadata->optInTbgEmail) ||
+            empty($event->data->object->id)
+        );
+        if ($missingRequiredField) {
+            $message = 'Hook missing required values';
+            $this->logger->warning("Donation ID {$event->data->object->id}: {$message}");
+            $error = new ActionError(ActionError::BAD_REQUEST, $message);
+
+            return $this->respond(new ActionPayload(400, null, $error));
+        }
+
+        $donation->setDonationStatus($event->status);
+        $donation->setDonorBillingAddress($event->data->object->billing_details->address->postal_code);
+        $donation->setDonorCountryCode($event->data->object->billing_details->address->country);
+        $donation->setDonorEmailAddress($donationData->emailAddress);
+        $donation->setDonorFirstName($event->data->object->billing_details->name);
+        $donation->setGiftAid($event->data->metadata->coreDonationGiftAid);
+        $donation->setTbgComms($event->data->metadata->optInTbgEmail);
+        $donation->setTransactionId($event->data->object->id);
+
+        if (isset($event->data->metadata->tipAmount)) {
+            $donation->setTipAmount((string) $event->data->metadata->tipAmount);
+        }
+
+        if ($donation->isReversed() && $event->data->metadata->matchedAmount > 0) {
+            $this->donationRepository->releaseMatchFunds($donation);
+        }
+
+        $this->entityManager->persist($donation);
+
+        // We log if this fails but don't worry the webhook-sending payment client
+        // about it. We'll re-try sending the updated status to Salesforce in a future
+        // batch sync.
+        $this->donationRepository->push($donation, false); // Attempt immediate sync to Salesforce
 
         return $this->respondWithData($event->data->object);
     }
