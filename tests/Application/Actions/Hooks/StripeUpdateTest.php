5783ac4b59f898cf4cb7c976c4940a2db8c33587
tbg-jasonfung <jason@thebiggive.org.uk>
2020-08-18 14:47:19 +0100

    MAT-105 begin stripe update unit test

---
+++ tests/Application/Actions/Hooks/StripeUpdateTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Tests\Application\Actions\Hooks;
+
+use DI\Container;
+use Doctrine\ORM\EntityManagerInterface;
+use MatchBot\Application\Actions\ActionPayload;
+use MatchBot\Application\HttpModels\Donation as HttpDonation;
+use MatchBot\Domain\Donation;
+use MatchBot\Domain\DonationRepository;
+use MatchBot\Domain\StripeWebhook;
+use MatchBot\Tests\Application\Actions\DonationTestDataTrait;
+use MatchBot\Tests\TestCase;
+use Prophecy\Argument;
+use Slim\Exception\HttpNotFoundException;
+
+class StripeUpdateTest extends TestCase
+{
+    use DonationTestDataTrait;
+
+    public function testUnsupportedAction(): void
+    {
+        $app = $this->getAppInstance();
+        /** @var Container $container */
+        $container = $app->getContainer();
+
+        $body = file_get_contents(dirname(__DIR__, 3) . '/TestData/canceled.json');
+
+        $stripeRepoProphecy = $this->prophesize(StripeWebhook::class);
+        $entityManagerProphecy = $this->prophesize(EntityManagerInterface::class);
+        $donationRepoProphecy = $this->prophesize(DonationRepository::class);
+
+        $container->set(StripeWebhook::class, $stripeRepoProphecy->reveal());
+        $container->set(EntityManagerInterface::class, $entityManagerProphecy->reveal());
+        $container->set(DonationRepository::class, $donationRepoProphecy->reveal());
+
+        $stripeSignature = 't=' . time() . 'v1=' . $this->getValidAuth($body) . 'v0=' . $this->getValidAuth($body);
+
+        $request = $this->createRequest('POST', '/hooks/stripe', $body)
+            ->withHeader('stripe-signature', $stripeSignature);
+        
+        $response = $app->handle($request);
+
+        $payload = (string) $response->getBody();
+
+        $expectedPayload = new ActionPayload(400, ['error' => 'Unsupported Action']);
+        $expectedSerialised = json_encode($expectedPayload, JSON_PRETTY_PRINT);
+
+        $this->assertEquals($expectedSerialised, $payload);
+        $this->assertEquals(400, $response->getStatusCode());
+    }
+
+    private function getValidAuth(string $body): string
+    {
+        return hash_hmac('sha256', $body, getenv('STRIPE_WEBHOOK_SIGNING_SECRET'));
+    }
+}
