94583e1630341c08ac71e97a792c82c1aea4dcf7
tbg-jasonfung <jason@thebiggive.org.uk>
2020-09-02 12:57:25 +0100

    MAT-107 unit test invalid original donation status

--- tests/Application/Actions/Hooks/StripeUpdateTest.php
+++ tests/Application/Actions/Hooks/StripeUpdateTest.php
@@ -183,6 +183,56 @@ class StripeUpdateTest extends TestCase
         $this->assertEquals(204, $response->getStatusCode());
     }
 
+    public function testInvalidStatusPayout(): void
+    {
+        $app = $this->getAppInstance();
+        /** @var Container $container */
+        $container = $app->getContainer();
+
+        $body = file_get_contents(dirname(__DIR__, 3) . '/TestData/StripeWebhook/po_paid.json');
+        $balanceTxnResponse = file_get_contents(
+            dirname(__DIR__, 3) . '/TestData/StripeWebhook/ApiResponse/bt_success.json'
+        );
+        $donation = $this->getTestDonation();
+        $webhookSecret = $container->get('settings')['stripe']['webhookSecret'];
+        $time = (string) time();
+
+        $donation->setDonationStatus('Failed');
+
+        $entityManagerProphecy = $this->prophesize(EntityManagerInterface::class);
+
+        $stripeBalanceTransactionProphecy = $this->prophesize(BalanceTransactionService::class);
+        $stripeBalanceTransactionProphecy->all([
+            'limit' => 100,
+            'payout' => 'po_externalId_123',
+            'type' => 'charge'
+        ])
+            ->shouldBeCalledOnce()
+            ->willReturn(json_decode($balanceTxnResponse));
+
+        $donationRepoProphecy = $this->prophesize(DonationRepository::class);
+        $donationRepoProphecy
+            ->findOneBy(['chargeId' => 'ch_externalId_123'])
+            ->willReturn($donation)
+            ->shouldBeCalledOnce();
+
+        $stripeClientProphecy = $this->prophesize(StripeClient::class);
+        $stripeClientProphecy->balanceTransactions = $stripeBalanceTransactionProphecy->reveal();
+
+        $container->set(DonationRepository::class, $donationRepoProphecy->reveal());
+        $container->set(EntityManagerInterface::class, $entityManagerProphecy->reveal());
+        $container->set(StripeClient::class, $stripeClientProphecy->reveal());
+
+        $request = $this->createRequest('POST', '/hooks/stripe', $body)
+            ->withHeader('Stripe-Signature', $this->generateSignature($time, $body, $webhookSecret));
+
+        $app->handle($request);
+
+        // Despite handling Stripe payout logic, we expect donations
+        // that are not in 'Collected' status to remain the same.
+        $this->assertEquals('Failed', $donation->getDonationStatus());
+    }
+
     public function testSuccessfulPayout(): void
     {
         $app = $this->getAppInstance();
