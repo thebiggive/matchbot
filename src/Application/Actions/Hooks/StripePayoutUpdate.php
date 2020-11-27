a3d5312ca4902b21f06b6100efcb2e2b083dcc71
Noel Light-Hilary <noel@noellh.com>
2020-11-27 14:49:31 +0200

    MAT-124, MAT-136 - fix crash on manual Payout `payout.paid` webhook

--- src/Application/Actions/Hooks/StripePayoutUpdate.php
+++ src/Application/Actions/Hooks/StripePayoutUpdate.php
@@ -56,6 +56,14 @@ class StripePayoutUpdate extends Stripe
             $connectAccountId,
         ));
 
+        if (!$event->data->object->automatic) {
+            // If we try to use the `payout` filter attribute in the `balanceTransactions` call
+            // below in the manual payout case, Stripe errors out with "Balance transaction history
+            // can only be filtered on automatic transfers, not manual".
+            $this->logger->warning(sprintf('Skipping processing of manual Payout ID %s', $payoutId));
+            return $this->respond(new ActionPayload(204));
+        }
+
         $hasMore = true;
         $lastBalanceTransactionId = null;
         $paidChargeIds = [];
