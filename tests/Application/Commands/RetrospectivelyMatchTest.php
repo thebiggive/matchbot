c93643e1db9fedc4c024c9035cb5c6b52be4e8d6
tbg-jasonfung <jason@thebiggive.org.uk>
2021-01-07 14:17:39 +0000

    Fix tests using non numeric values

--- tests/Application/Commands/RetrospectivelyMatchTest.php
+++ tests/Application/Commands/RetrospectivelyMatchTest.php
@@ -58,7 +58,7 @@ class RetrospectivelyMatchTest extends TestCase
     public function testNonWholeDaysBackIsRounded(): void
     {
         $commandTester = new CommandTester($this->command);
-        $commandTester->execute(['days-back' => '7.5']);
+        $commandTester->execute(['days-back' => 7.5]);
 
         $expectedOutputLines = [
             'matchbot:retrospectively-match starting!',
@@ -73,7 +73,7 @@ class RetrospectivelyMatchTest extends TestCase
     public function testWholeDaysBackProceeds(): void
     {
         $commandTester = new CommandTester($this->command);
-        $commandTester->execute(['days-back' => '8']);
+        $commandTester->execute(['days-back' => 8]);
 
         $expectedOutputLines = [
             'matchbot:retrospectively-match starting!',
