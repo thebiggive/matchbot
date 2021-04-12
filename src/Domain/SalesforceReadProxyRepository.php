7a5f91546d5b90065d3d3edb771b5954f37380b1
Noel Light-Hilary <noel@noellh.com>
2021-04-12 10:50:07 +0100

    MAT-161 - lint fix

--- src/Domain/SalesforceReadProxyRepository.php
+++ src/Domain/SalesforceReadProxyRepository.php
@@ -24,7 +24,10 @@ abstract class SalesforceReadProxyRepository extends SalesforceProxyRepository
     {
         // Make sure we update existing object if passed in a partial copy and we already have that Salesforce object
         // persisted, otherwise we'll try to insert a duplicate and get an ORM crash.
-        if (!$proxy->hasBeenPersisted() && ($existingProxy = $this->findOneBy(['salesforceId' => $proxy->getSalesforceId()]))) {
+        if (
+            !$proxy->hasBeenPersisted() &&
+            ($existingProxy = $this->findOneBy(['salesforceId' => $proxy->getSalesforceId()]))
+        ) {
             $this->logInfo('Updating ' . get_class($proxy) . ' ' . $proxy->getSalesforceId() . '...');
             $proxy = $existingProxy;
         } else {
