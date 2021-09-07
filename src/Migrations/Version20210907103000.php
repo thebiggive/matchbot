f4967f3a7b4c3a083175c8fb3bde424732b60d30
Noel Light-Hilary <noel@noellh.com>
2021-09-07 11:32:53 +0100

    MAT-179 - add fee cover field
    Doing this in a separate step ahead of logic changes so deploys can roll out safely

---
+++ src/Migrations/Version20210907103000.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Add Donation.feeCoverAmount
+ */
+final class Version20210907103000 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Add Donation.feeCoverAmount';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("ALTER TABLE Donation ADD feeCoverAmount NUMERIC(18, 2) DEFAULT '0.00' NOT NULL");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Donation DROP feeCoverAmount');
+    }
+}
