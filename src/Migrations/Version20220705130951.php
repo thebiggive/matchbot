fbc92d96b207bada16057ebac4aca54ff21732e2
Noel Light-Hilary <noel@noellh.com>
2022-07-05 14:00:49 +0100

    MAT-253 – set 27 June-claimed donation to get Gift Aid via the new Redis queue

---
+++ src/Migrations/Version20220705130951.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * MAT-253 re-queue end of June donations sent to old SQS queue.
+ */
+final class Version20220705130951 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Re-queue end of June donations sent to old SQS queue';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<EOT
+UPDATE Donation
+SET
+    tbgGiftAidRequestQueuedAt = NULL,
+    tbgGiftAidRequestFailedAt = NULL,
+    tbgGiftAidRequestConfirmedCompleteAt = NULL,
+    tbgGiftAidRequestCorrelationId = NULL,
+    tbgGiftAidResponseDetail = NULL
+WHERE
+    tbgGiftAidRequestQueuedAt BETWEEN '2022-06-27 00:00:00' AND '2022-06-27 23:59:59' AND
+    tbgGiftAidRequestCorrelationId IS NULL AND
+    tbgGiftAidRequestFailedAt IS NULL
+LIMIT 148
+EOT);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // No un-patch.
+    }
+}
