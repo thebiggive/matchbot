7e6bea5b56962f7a0103520ff29958608c600612
Noel Light-Hilary <noel@noellh.com>
2022-10-30 10:00:27 +0000

    MAT-267 – populate remaining `paymentMethodType`s

---
+++ src/Migrations/Version20221030095831.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Fix remaining missing payment method types following field addition.
+ */
+final class Version20221030095831 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Fix remaining missing payment method types following field addition';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("UPDATE Donation SET paymentMethodType = :card WHERE paymentMethodType IS NULL", ['card' => 'card']);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // No un-patch
+    }
+}
