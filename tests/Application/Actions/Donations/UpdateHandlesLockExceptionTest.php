b51362598b2c53902a6e519f34d6c46cfddcb448
Barney Laurance <barney@thebiggive.org.uk>
2023-01-11 16:19:29 +0000

    MAT-280: WIP retry requests for cancellations as well as other donation updates

--- tests/Application/Actions/Donations/UpdateHandlesLockExceptionTest.php
+++ tests/Application/Actions/Donations/UpdateHandlesLockExceptionTest.php
@@ -15,6 +15,7 @@ use MatchBot\Domain\Charity;
 use MatchBot\Domain\Donation;
 use MatchBot\Domain\DonationRepository;
 use PHPUnit\Framework\MockObject\Builder\InvocationMocker;
+use Prophecy\Argument;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Psr\Log\NullLogger;
 use Ramsey\Uuid\Uuid;
@@ -31,7 +32,10 @@ class UpdateHandlesLockExceptionTest extends \PHPUnit\Framework\TestCase
 
     private int $alreadyThrewTimes = 0;
 
-    public function testFailsOnCancellationLockException(): void
+    /**
+     * @dataProvider newStatuses
+     */
+    public function testRetriesOnUpdateLockException(string $newStatus): void
     {
         // arrange
         $donationId = 'donation_id';
@@ -45,8 +49,13 @@ class UpdateHandlesLockExceptionTest extends \PHPUnit\Framework\TestCase
 
         $donationRepositoryProphecy->findAndLockOneBy(['uuid' => $donationId])->willReturn($donation);
 
-        $donationRepositoryProphecy->push($donation, false)
-            ->willThrow(new LockWaitTimeoutException($this->createStub(DriverException::class), null));
+
+        if ($newStatus == "Cancelled") {
+            $donationRepositoryProphecy->push($donation, false)->shouldBeCalled()->willReturn(true);
+            $donationRepositoryProphecy->releaseMatchFunds($donation)->shouldBeCalled();
+        } else {
+            $donationRepositoryProphecy->deriveFees($donation, 'some-card-brand', 'some-country')->shouldBeCalled()->willReturn($donation);
+        }
 
         $updateAction = new Update(
             $donationRepositoryProphecy->reveal(),
@@ -56,52 +65,7 @@ class UpdateHandlesLockExceptionTest extends \PHPUnit\Framework\TestCase
             new NullLogger()
         );
 
-        $request = new ServerRequest(method: 'PUT', uri: '', body: '{"status": "Cancelled"}');
-
-        // act
-        $response = $updateAction($request, new Response(), ['donationId' => $donationId]);
-
-        // assert
-        $this->assertSame(400, $response->getStatusCode());
-        $this->assertJsonStringEqualsJsonString(
-            <<<'JSON'
-              {
-                "error": {
-                  "type": "BAD_REQUEST",
-                  "description": "Cancellation was not possible due to another action at the same time affecting this donation"
-                }
-              }
-            JSON
-            ,
-            (string)$response->getBody()
-        );
-    }
-
-    public function testRetriesOnUpdateLockException(): void
-    {
-        // arrange
-        $donationId = 'donation_id';
-
-        $donationRepositoryProphecy = $this->prophesize(DonationRepository::class);
-        $entityManagerProphecy = $this->prophesize(EntityManagerInterface::class);
-        $stripeIntentsProphecy = $this->prophesize(PaymentIntentService::class);
-        $fakeStripeClient = $this->fakeStripeClient($stripeIntentsProphecy->reveal());
-
-        $donation = $this->getDonation();
-
-        $donationRepositoryProphecy->findAndLockOneBy(['uuid' => $donationId])->willReturn($donation);
-        $donationRepositoryProphecy->deriveFees($donation, 'some-card-brand', 'some-country')->shouldBeCalled()->willReturn($donation);
-
-
-        $updateAction = new Update(
-            $donationRepositoryProphecy->reveal(),
-            $entityManagerProphecy->reveal(),
-            new Serializer([new ObjectNormalizer()], [new JsonEncoder()]),
-            $fakeStripeClient,
-            new NullLogger()
-        );
-
-        $request = new ServerRequest(method: 'PUT', uri: '', body: $this->putRequestBody());
+        $request = new ServerRequest(method: 'PUT', uri: '', body: $this->putRequestBody($newStatus));
 
         $testCase = $this; // prophecy rebinds $this to point to the test double in the closure
         $entityManagerProphecy->flush()->will(function () use ($testCase) {
@@ -113,8 +77,13 @@ class UpdateHandlesLockExceptionTest extends \PHPUnit\Framework\TestCase
         });
 
         $entityManagerProphecy->beginTransaction()->shouldBeCalled();
-        $entityManagerProphecy->refresh($donation, LockMode::PESSIMISTIC_WRITE)->shouldBeCalled();
+        $entityManagerProphecy->refresh($donation, LockMode::PESSIMISTIC_WRITE)->shouldBeCalled()
+            ->will(function () use ($donation) {
+                $donation->setDonationStatus('Pending'); // simulate refreshing donation from DB.
+            })
+        ;
         $entityManagerProphecy->persist($donation)->shouldBeCalled();
+        $entityManagerProphecy->rollback()->willThrow(\Exception::class);
         $entityManagerProphecy->commit()->shouldBeCalled();
 
         // act
@@ -189,17 +158,16 @@ class UpdateHandlesLockExceptionTest extends \PHPUnit\Framework\TestCase
         $donation->setCurrencyCode('GBP');
         $donation->setAmount('1');
         $donation->setUuid(Uuid::uuid4());
-        $donation->setDonationStatus('Pending');
         $donation->setDonorFirstName('Donor first name');
         $donation->setDonorLastName('Donor last name');
 
         return $donation;
     }
 
-    private function putRequestBody(): string
+    private function putRequestBody(string $newStatus): string
     {
         // props in alphabetical order
-        return <<<'JSON'
+        return <<<JSON
         {
           "autoConfirmFromCashBalance": false,
           "billingPostalAddress": null,
@@ -219,11 +187,19 @@ class UpdateHandlesLockExceptionTest extends \PHPUnit\Framework\TestCase
           "optInChampionEmail": false,
           "optInCharityEmail": false,
           "optInTbgEmail": false,
-          "status": "Pending",
+          "status": "$newStatus",
           "tipAmount": null,
           "tipGiftAid": null,
           "transactionId": null
         }
         JSON;
+    }
+
+    public function newStatuses(): array
+    {
+        return [
+            ['Pending'],
+            ['Cancelled'],
+        ];
     }
 }
