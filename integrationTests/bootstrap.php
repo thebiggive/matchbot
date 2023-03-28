79a4a8200ee57d83cf33ed686e84951b24a4bf93
Barney Laurance <barney@thebiggive.org.uk>
2023-03-28 16:02:19 +0100

    Block integraiton tests in prod
    No reason these would ever happen unless we make a mistake

--- integrationTests/bootstrap.php
+++ integrationTests/bootstrap.php
@@ -5,6 +5,10 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+if (! in_array(getenv('APP_ENV'), ['local', 'test'])) {
+    throw new \Exception("Don't run integration tests in live!");
+}
+
 $container = require __DIR__ . '/../bootstrap.php';
 IntegrationTest::setContainer($container);
 
