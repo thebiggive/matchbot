08b59994eea34bf2a9f340f760bbead05b5d3790
Barney Laurance <barney@thebiggive.org.uk>
2023-04-14 10:48:11 +0100

    MAT-295: Simplify unit test

--- tests/Application/Commands/DeleteStalePaymentDetailsTest.php
+++ tests/Application/Commands/DeleteStalePaymentDetailsTest.php
@@ -25,18 +25,16 @@ class DeleteStalePaymentDetailsTest extends TestCase
 
     public function testDeletingOneStaleCard(): void
     {
-        $initDate = new \DateTimeImmutable('now');
+        // arrange
+        $initDate = new \DateTimeImmutable('2023-04-10T00:00:00+0100');
+        $previousDay = new \DateTimeImmutable('2023-04-09T00:00:00+0100');
+
         $testCustomerId = 'cus_aaaaaaaaaaaa11';
         $testPaymentMethodId = 'pm_aaaaaaaaaaaa13';
-        $testPaymentMethodFingerprint = 'Xt5EWLLDS7FJjR1c';
 
         $stripeChargesProphecy = $this->prophesize(ChargeService::class);
         $stripeChargesProphecy->search([
-            'query' => sprintf(
-                'customer:"%s" and payment_method_details.card.fingerprint:"%s" and status:"succeeded"',
-                $testCustomerId,
-                $testPaymentMethodFingerprint,
-            ),
+            'query' => 'customer:"cus_aaaaaaaaaaaa11" and payment_method_details.card.fingerprint:"TEST_CARD_FINGERPRINT" and status:"succeeded"',
             'limit' => 100,
         ])
             ->shouldBeCalledOnce()
@@ -53,18 +51,9 @@ class DeleteStalePaymentDetailsTest extends TestCase
                 $this->getStripeHookMock('ApiResponse/pm'),
             ));
 
-        // One PM should be detached i.e. soft deleted.
-        $stripePaymentMethodsProphecy->detach($testPaymentMethodId)
-            ->shouldBeCalledOnce()
-            ->willReturn($this->getStripeHookMock('ApiResponse/pm'));
-
-        $oneDayBeforeTest = $initDate
-            ->sub(new \DateInterval('P1D'))
-            ->getTimestamp();
-
         $stripeCustomersProphecy = $this->prophesize(CustomerService::class);
         $stripeCustomersProphecy->search([
-            'query' => "created<$oneDayBeforeTest and metadata['hasPasswordSince']:null",
+            'query' => "created<{$previousDay->getTimestamp()} and metadata['hasPasswordSince']:null",
             'limit' => 100,
         ])
             ->shouldBeCalledOnce()
@@ -82,8 +71,17 @@ class DeleteStalePaymentDetailsTest extends TestCase
             $stripeClientProphecy,
             $initDate,
         ));
+
+        // assert
+        // One PM should be detached i.e. soft deleted.
+        $stripePaymentMethodsProphecy->detach($testPaymentMethodId)
+            ->shouldBeCalledOnce()
+            ->willReturn($this->getStripeHookMock('ApiResponse/pm'));
+
+        // act
         $commandTester->execute([]);
 
+        // assert
         $expectedOutputLines = [
             'matchbot:delete-stale-payment-details starting!',
             'Deleted 1 payment methods from Stripe, having checked 1 customers',
@@ -96,17 +94,15 @@ class DeleteStalePaymentDetailsTest extends TestCase
 
     public function testNotDeletingOneUsedCard(): void
     {
-        $initDate = new \DateTimeImmutable('now');
+        // arrange
+        $initDate = new \DateTimeImmutable('2023-04-10T00:00:00+0100');
+        $previousDay = new \DateTimeImmutable('2023-04-09T00:00:00+0100');
+
         $testCustomerId = 'cus_aaaaaaaaaaaa11';
-        $testPaymentMethodFingerprint = 'Xt5EWLLDS7FJjR1c';
 
         $stripeChargesProphecy = $this->prophesize(ChargeService::class);
         $stripeChargesProphecy->search([
-            'query' => sprintf(
-                'customer:"%s" and payment_method_details.card.fingerprint:"%s" and status:"succeeded"',
-                $testCustomerId,
-                $testPaymentMethodFingerprint,
-            ),
+            'query' => 'customer:"cus_aaaaaaaaaaaa11" and payment_method_details.card.fingerprint:"TEST_CARD_FINGERPRINT" and status:"succeeded"',
             'limit' => 100,
         ])
             ->shouldBeCalledOnce()
@@ -123,17 +119,9 @@ class DeleteStalePaymentDetailsTest extends TestCase
                 $this->getStripeHookMock('ApiResponse/pm'),
             ));
 
-        // *No* PM should be detached i.e. soft deleted, with any ID.
-        $stripePaymentMethodsProphecy->detach(Argument::type('string'))
-            ->shouldNotBeCalled();
-
-        $oneDayBeforeTest = $initDate
-            ->sub(new \DateInterval('P1D'))
-            ->getTimestamp();
-
         $stripeCustomersProphecy = $this->prophesize(CustomerService::class);
         $stripeCustomersProphecy->search([
-            'query' => "created<$oneDayBeforeTest and metadata['hasPasswordSince']:null",
+            'query' => "created<{$previousDay->getTimestamp()} and metadata['hasPasswordSince']:null",
             'limit' => 100,
         ])
             ->shouldBeCalledOnce()
@@ -151,14 +139,22 @@ class DeleteStalePaymentDetailsTest extends TestCase
             $stripeClientProphecy,
             $initDate,
         ));
+
+        // assert
+        // *No* PM should be detached i.e. soft deleted, with any ID.
+        $stripePaymentMethodsProphecy->detach(Argument::type('string'))
+            ->shouldNotBeCalled();
+
+        // act
         $commandTester->execute([]);
 
-        $expectedOutputLines = [
-            'matchbot:delete-stale-payment-details starting!',
-            'Deleted 0 payment methods from Stripe, having checked 1 customers',
-            'matchbot:delete-stale-payment-details complete!',
-        ];
-        $this->assertEquals(implode("\n", $expectedOutputLines) . "\n", $commandTester->getDisplay());
+        // assert
+        $this->assertEquals(<<<EXPECTED
+            matchbot:delete-stale-payment-details starting!
+            Deleted 0 payment methods from Stripe, having checked 1 customers
+            matchbot:delete-stale-payment-details complete!
+            
+            EXPECTED, $commandTester->getDisplay());
 
         $this->assertEquals(0, $commandTester->getStatusCode());
     }
