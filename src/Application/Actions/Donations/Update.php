b1f83e47b0c7ee3fd638888fd91b69509b26d365
Noel Light-Hilary <noel@noellh.com>
2023-05-31 16:31:23 +0100

    MAT-301 – remove misleading new comment

--- src/Application/Actions/Donations/Update.php
+++ src/Application/Actions/Donations/Update.php
@@ -299,9 +299,6 @@ class Update extends Action
             }
         }
 
-        // We hold off finishing the transaction for now, because the current logic
-        // involves rolling back our whole Update in *some* Stripe error scenarios.
-        // We don't have enough info to detect these without making the Stripe API call.
         $this->save($donation);
 
         return $this->respondWithData($response, $donation->toApiModel());
