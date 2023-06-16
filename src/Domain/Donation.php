999b8bf0439c233fca02059287bae1ac7717605f
Noel Light-Hilary <noel@noellh.com>
2023-06-16 08:22:43 +0100

    Fix checking for tips under £1
    And tidy up comparison operator + types

--- src/Domain/Donation.php
+++ src/Domain/Donation.php
@@ -841,13 +841,14 @@ class Donation extends SalesforceWriteProxy
         return $this->tipAmount;
     }
 
+    /**
+     * @param string $tipAmount Numeric string
+     */
     public function setTipAmount(string $tipAmount): void
     {
-        /** @var numeric-string $tipAmount */
-
         if (
-            $this->paymentMethodType == PaymentMethodType::CustomerBalance &&
-            bccomp($tipAmount, '0') !== 0
+            $this->paymentMethodType === PaymentMethodType::CustomerBalance &&
+            bccomp($tipAmount, '0', 2) !== 0
         ) {
             // We would have accepted a tip at the time the customer balance was created, so we don't take a second
             // tip as part of the donation.
