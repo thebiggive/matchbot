25912a1b16909370fae158c76ca3901dda0d530d
Noel Light-Hilary <noel@noellh.com>
2023-07-07 11:14:12 +0100

    MAT-303 – mark one campaign for a new Gift Aid claim
    Following processing errors with the first claim attempt

---
+++ src/Migrations/Version20230707100241.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * MAT-303 Gift Aid re-claim
+ */
+final class Version20230707100241 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Re-claim Gift Aid for a charity who were incorrectly enrolled the first time';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<SQL
+            UPDATE Donation
+            SET
+                tbgGiftAidRequestQueuedAt = NULL,
+                tbgGiftAidRequestFailedAt = NULL,
+                tbgGiftAidRequestConfirmedCompleteAt = NULL,
+                tbgGiftAidRequestCorrelationId = NULL,
+                tbgGiftAidResponseDetail = NULL
+            WHERE
+                campaign_id = 4635 AND
+                giftAid = 1 AND
+                tbgShouldProcessGiftAid = 1 AND
+                tbgGiftAidRequestCorrelationId IS NOT NULL AND
+                donationStatus = 'Paid'
+            LIMIT 224;
+        SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // No un-patch
+    }
+}
