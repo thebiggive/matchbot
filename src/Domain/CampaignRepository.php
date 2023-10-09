thebiggive/matchbot

commit 037a7cf224356bbb64c4672e44bb6ad6ecb9b71f

MAT-321: Fix crash during Donation Create persistence retry, due to missing charity name
Introducing a constructor for Charity that requires all the parameters
that we would pass to the updateFromSfPUll function

src/Domain/CampaignRepository.php
@@ -91,26 +91,33 @@ class CampaignRepository extends SalesforceReadProxyRepository
         ?string $stripeAccountId,
         ?string $giftAidOnboardingStatus,
         ?string $hmrcReferenceNumber,
-        ?string $regulator,
+        string $regulator,
         ?string $regulatorNumber,
     ): Charity {
         $charity = $this->getEntityManager()
             ->getRepository(Charity::class)
             ->findOneBy(['salesforceId' => $salesforceCharityId]);
         if (!$charity) {
-            $charity = new Charity();
-            $charity->setSalesforceId($salesforceCharityId);
+            $charity = new Charity(
+                charityName: $charityName,
+                stripeAccountId: $stripeAccountId,
+                hmrcReferenceNumber: $hmrcReferenceNumber,
+                giftAidOnboardingStatus: $giftAidOnboardingStatus,
+                regulator: $this->getRegulatorHMRCIdentifier($regulator),
+                regulatorNumber: $regulatorNumber,
+                time: new \DateTime('now'),
+            );
+        } else {
+            $charity->updateFromSfPull(
+                charityName: $charityName,
+                stripeAccountId: $stripeAccountId,
+                hmrcReferenceNumber: $hmrcReferenceNumber,
+                giftAidOnboardingStatus: $giftAidOnboardingStatus,
+                regulator: $this->getRegulatorHMRCIdentifier($regulator),
+                regulatorNumber: $regulatorNumber,
+                time: new \DateTime('now'),
+            );
         }
-
-        $charity->updateFromSfPull(
-            charityName: $charityName,
-            stripeAccountId: $stripeAccountId,
-            hmrcReferenceNumber: $hmrcReferenceNumber,
-            giftAidOnboardingStatus: $giftAidOnboardingStatus,
-            regulator: $this->getRegulatorHMRCIdentifier($regulator),
-            regulatorNumber: $regulatorNumber,
-            time: new \DateTime('now'),
-        );
 
         $this->getEntityManager()->persist($charity);
 

src/Domain/Charity.php
@@ -38,9 +38,9 @@ class Charity extends SalesforceReadProxy
 
     /**
      * @ORM\Column(type="string")
-     * @var string|null
+     * @var string
      */
-    protected ?string $name = null;
+    protected string $name;
 
     /**
      * @ORM\Column(type="string", length=255, unique=true, nullable=true)
@@ -87,6 +87,31 @@ class Charity extends SalesforceReadProxy
      */
     private bool $tbgApprovedToClaimGiftAid = false;
 
+    public function __construct(
+        string $charityName,
+        ?string $stripeAccountId,
+        ?string $hmrcReferenceNumber,
+        ?string $giftAidOnboardingStatus,
+        ?string $regulator,
+        ?string $regulatorNumber,
+        DateTime $time,
+    )
+    {
+        $this->updatedAt = $time;
+        $this->createdAt = $time;
+
+        // every charity originates as pulled from SF.
+        $this->updateFromSfPull(
+            charityName: $charityName,
+            stripeAccountId: $stripeAccountId,
+            hmrcReferenceNumber: $hmrcReferenceNumber,
+            giftAidOnboardingStatus: $giftAidOnboardingStatus,
+            regulator: $regulator,
+            regulatorNumber: $regulatorNumber,
+            time: new \DateTime('now'),
+        );
+    }
+
     /**
      * @param string $name
      */
@@ -95,7 +120,7 @@ class Charity extends SalesforceReadProxy
         $this->name = $name;
     }
 
-    public function getName(): ?string
+    public function getName(): string
     {
         return $this->name;
     }

tests/TestCase.php
@@ -172,6 +172,31 @@ class TestCase extends PHPUnitTestCase
 
     protected function getMinimalCampaign(): Campaign
     {
-        return new Campaign(new Charity());
+        return new Campaign(\MatchBot\Tests\TestCase::someCharity());
+    }
+
+    /**
+     * Returns some random charity - use if you don't care about the details or will replace them with setters later.
+     * Introduced to replace many old calls to instantiate Charity with zero arguments.
+     */
+    public static function someCharity(): Charity
+    {
+        return new Charity(
+            charityName: "Charity Name",
+            stripeAccountId: "stripe-account-id-" . self::randomHex(),
+            hmrcReferenceNumber: 'H' . self::randomHex(3),
+            giftAidOnboardingStatus: 'Onboarded',
+            regulator: 'CCEW',
+            regulatorNumber: 'Reg-no',
+            time: new \DateTime('2023-10-06T18:51:27'),
+        );
+    }
+
+    /**
+     * @param positive-int $num_bytes
+     */
+    private static function randomHex(int $num_bytes=8): string
+    {
+        return bin2hex(random_bytes($num_bytes));
     }
 }

integrationTests/CharityRepositoryTest.php
@@ -27,7 +27,7 @@ class CharityRepositoryTest extends IntegrationTest
         // arrange
         $sut = $this->getService(CharityRepository::class);
 
-        $charity = new Charity();
+        $charity = \MatchBot\Tests\TestCase::someCharity();
         $charity->setName("Charity Name");
 
         $em = $this->getService(EntityManagerInterface::class);

integrationTests/DonationRepositoryTest.php
@@ -43,7 +43,7 @@ class DonationRepositoryTest extends IntegrationTest
 
     private function prepareOnboardedCharity(bool $withAgentApproval): Charity
     {
-        $charity = new Charity();
+        $charity = \MatchBot\Tests\TestCase::someCharity();
         $charity->setName('Charity Name');
         $charity->setHmrcReferenceNumber('any-ref');
         $charity->setTbgClaimingGiftAid(true);

psalm-baseline.xml
@@ -894,17 +894,9 @@
     <ParamNameMismatch>
       <code>$campaign</code>
     </ParamNameMismatch>
-    <PossiblyNullArgument>
-      <code>$regulator</code>
-    </PossiblyNullArgument>
     <UndefinedMethod>
       <code>getById</code>
     </UndefinedMethod>
-  </file>
-  <file src="src/Domain/Charity.php">
-    <MissingConstructor>
-      <code>$name</code>
-    </MissingConstructor>
   </file>
   <file src="src/Domain/CommandLockKeys.php">
     <MissingConstructor>
@@ -973,12 +965,6 @@
       <code>Donation</code>
       <code>Donation</code>
     </PropertyNotSetInConstructor>
-    <RedundantCondition>
-      <code><![CDATA[$this->getCampaign()->getCharity()->getName()]]></code>
-    </RedundantCondition>
-    <TypeDoesNotContainNull>
-      <code><![CDATA['']]></code>
-    </TypeDoesNotContainNull>
   </file>
   <file src="src/Domain/DonationRepository.php">
     <ArgumentTypeCoercion>

src/Application/Actions/Donations/Create.php
@@ -213,7 +213,7 @@ class Create extends Action
                     $exception->getStripeCode() ?? 'unknown',
                     get_class($exception),
                     $message,
-                    $donation->getCampaign()->getCharity()->getName() ?? 'unknown',
+                    $donation->getCampaign()->getCharity()->getName(),
                     $donation->getCampaign()->getCharity()->getStripeAccountId() ?? 'unknown',
                 ));
                 $error = new ActionError(ActionError::SERVER_ERROR, 'Could not make Stripe Payment Intent (B)');
@@ -241,9 +241,6 @@ class Create extends Action
         $maximumLength = 22; // https://stripe.com/docs/payments/payment-intents#dynamic-statement-descriptor
         $prefix = 'Big Give ';
 
-        /**
-         * @psalm-suppress PossiblyNullArgument We don't expect to get here with no charity » let it crash for now.
-         */
         return $prefix . mb_substr(
             $this->removeSpecialChars($charity->getName()),
             0,

src/Domain/Donation.php
@@ -368,7 +368,7 @@ class Donation extends SalesforceWriteProxy
 
     public function __toString(): string
     {
-        $charityName = $this->getCampaign()->getCharity()->getName() ?? '[pending charity]';
+        $charityName = $this->getCampaign()->getCharity()->getName();
         return "Donation {$this->getUuid()} to $charityName";
     }
 
@@ -1251,7 +1251,7 @@ class Donation extends SalesforceWriteProxy
         $donationMessage->amount = (float) $this->amount;
 
         $donationMessage->org_hmrc_ref = $this->getCampaign()->getCharity()->getHmrcReferenceNumber() ?? '';
-        $donationMessage->org_name = $this->getCampaign()->getCharity()->getName() ?? '';
+        $donationMessage->org_name = $this->getCampaign()->getCharity()->getName();
         $donationMessage->org_regulator = $this->getCampaign()->getCharity()->getRegulator();
         $donationMessage->org_regulator_number = $this->getCampaign()->getCharity()->getRegulatorNumber();
 

commit 4cec3c77d77c21d71cd3c018cd346c2568a3c44a

MAT-321: Fix bug introduced in commit 037a7cf22
SF ID has to be set for all charities. The DB doesn't enforce this, but
if we save a charity with no SF ID then we won't find it when we need it
and we'll try to pull it from SF again - and get a constraint violation
error on the duplicate Stripe ID.

This bug has not been in production - I found it on my local

src/Domain/CampaignRepository.php
@@ -94,11 +94,13 @@ class CampaignRepository extends SalesforceReadProxyRepository
         string $regulator,
         ?string $regulatorNumber,
     ): Charity {
+        $this->logger->info("Searching for charity by sf ID: $salesforceCharityId");
         $charity = $this->getEntityManager()
             ->getRepository(Charity::class)
             ->findOneBy(['salesforceId' => $salesforceCharityId]);
         if (!$charity) {
             $charity = new Charity(
+                salesforceId: $salesforceCharityId,
                 charityName: $charityName,
                 stripeAccountId: $stripeAccountId,
                 hmrcReferenceNumber: $hmrcReferenceNumber,

src/Domain/Charity.php
@@ -88,6 +88,7 @@ class Charity extends SalesforceReadProxy
     private bool $tbgApprovedToClaimGiftAid = false;
 
     public function __construct(
+        string $salesforceId,
         string $charityName,
         ?string $stripeAccountId,
         ?string $hmrcReferenceNumber,
@@ -99,6 +100,7 @@ class Charity extends SalesforceReadProxy
     {
         $this->updatedAt = $time;
         $this->createdAt = $time;
+        $this->setSalesforceId($salesforceId);
 
         // every charity originates as pulled from SF.
         $this->updateFromSfPull(

tests/TestCase.php
@@ -182,6 +182,7 @@ class TestCase extends PHPUnitTestCase
     public static function someCharity(): Charity
     {
         return new Charity(
+            salesforceId: '12CharityId_' .  self::randomHex(3),
             charityName: "Charity Name",
             stripeAccountId: "stripe-account-id-" . self::randomHex(),
             hmrcReferenceNumber: 'H' . self::randomHex(3),