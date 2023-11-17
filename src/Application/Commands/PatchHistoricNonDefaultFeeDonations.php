1e379389d6f794ed854c81e7321a19d374d23d54
Barney Laurance <barney@thebiggive.org.uk>
2023-11-17 17:38:45 +0000

    MAT-331: Only push donation fee patch updates to SF when there is actually a change

--- src/Application/Commands/PatchHistoricNonDefaultFeeDonations.php
+++ src/Application/Commands/PatchHistoricNonDefaultFeeDonations.php
@@ -60,11 +60,10 @@ class PatchHistoricNonDefaultFeeDonations extends Command
                 'donationUuid' => $donation['uuid'],
                 'charityFee' => $stripeFeeRechargeNet,
                 'charityFeeVat' => $stripeFeeRechargeVat,
-                'salesforcePushStatus' => SalesforceWriteProxy::PUSH_STATUS_PENDING_UPDATE,
             ];
             $rowsAffected = $this->connection->executeStatement(
                 <<<'SQL'
-                    UPDATE Donation set charityFee = :charityFee, charityFeeVat = :charityFeeVat, salesforcePushStatus = :salesforcePushStatus
+                    UPDATE Donation set charityFee = :charityFee, charityFeeVat = :charityFeeVat
                                     WHERE uuid = :donationUuid
                                     LIMIT 1
                     SQL
@@ -77,6 +76,21 @@ class PatchHistoricNonDefaultFeeDonations extends Command
                 0 => $output->writeln("Donation data already matches stripe, nothing to update"),
                 1 => $output->writeln("Donation data updated: uuid: {$donation['uuid']}, charityFee $stripeFeeRechargeNet, charityFeeVat $stripeFeeRechargeVat"),
             };
+
+            if ($rowsAffected === 1) {
+                $this->connection->executeStatement(
+                    <<<'SQL'
+                    UPDATE Donation set salesforcePushStatus = :salesforcePushStatus
+                                    WHERE uuid = :donationUuid
+                                    LIMIT 1
+                    SQL
+                    ,
+                    [
+                        'donationUuid' => $donation['uuid'],
+                        'salesforcePushStatus' => SalesforceWriteProxy::PUSH_STATUS_PENDING_UPDATE
+                    ]
+                );
+            }
 
             $output->writeln('');
         }
