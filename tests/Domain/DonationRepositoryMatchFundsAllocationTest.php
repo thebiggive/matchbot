05104e9b5593ff23910480a60fb10591055c2ade
Barney Laurance <barney@thebiggive.org.uk>
2023-11-23 14:47:34 +0000

    MAT-326: Unit test basic case of match funding £1 donation in DonationRepository

--- tests/Domain/DonationRepositoryMatchFundsAllocationTest.php
+++ tests/Domain/DonationRepositoryMatchFundsAllocationTest.php
@@ -8,6 +8,7 @@ use MatchBot\Domain\Campaign;
 use MatchBot\Domain\CampaignFunding;
 use MatchBot\Domain\Donation;
 use MatchBot\Domain\DonationRepository;
+use MatchBot\Domain\FundingWithdrawal;
 use MatchBot\Domain\PaymentMethodType;
 use MatchBot\Tests\Application\Matching\ArrayMatchingStorage;
 use PHPUnit\Framework\TestCase;
@@ -67,5 +68,63 @@ class DonationRepositoryMatchFundsAllocationTest extends TestCase
 
         // assert
         $this->assertSame('0.0', $amountMatched);
+    }
+
+    /**
+     * If the funds have £1 available, and the donation is for £1, then there should be £1 allocated.
+     */
+    public function testItAllocates1From1For1(): void
+    {
+        // arrange
+        $campaignFundingsRepositoryProphecy = $this->prophesize(\MatchBot\Domain\CampaignFundingRepository::class);
+
+        $emProphecy = $this->prophesize(\Doctrine\ORM\EntityManagerInterface::class);
+        $emProphecy->getRepository(CampaignFunding::class)->willReturn($campaignFundingsRepositoryProphecy->reveal());
+
+        $emProphecy->transactional(Argument::type(\Closure::class))->will(/**
+         * @param list<\Closure> $args
+         * @return mixed
+         */ fn(array $args) => $args[0]());
+        $matchingAdapter = new OptimisticRedisAdapter(new ArrayMatchingStorage(), $emProphecy->reveal(), new NullLogger());
+
+        $sut = new DonationRepository(
+            $emProphecy->reveal(),
+            new ClassMetadata(Donation::class),
+        );
+        $sut->setMatchingAdapter($matchingAdapter);
+        $sut->setLogger(new NullLogger());
+
+        $campaign = new Campaign(\MatchBot\Tests\TestCase::someCharity());
+
+        $campaignFunding = new CampaignFunding();
+        $campaignFunding->setCurrencyCode('GBP');
+        $campaignFunding->setAmountAvailable('1.0');
+
+        $availableFundings = [
+            $campaignFunding
+        ];
+        $campaignFundingsRepositoryProphecy->getAvailableFundings($campaign)->willReturn($availableFundings);
+        $emProphecy->persist($campaignFunding)->shouldBeCalled();
+        $emProphecy->persist(Argument::type(FundingWithdrawal::class))->shouldBeCalled();
+
+        $donation = Donation::fromApiModel(
+            new \MatchBot\Application\HttpModels\DonationCreate(
+                currencyCode: 'GBP',
+                donationAmount: '1',
+                projectId: "any project",
+                psp: 'stripe',
+                pspMethodType: PaymentMethodType::Card
+            ),
+            $campaign,
+        );
+
+        $emProphecy->persist($donation)->shouldBeCalled();
+        $emProphecy->flush()->shouldBeCalled();
+
+        // act
+        $amountMatched = $sut->allocateMatchFunds($donation);
+
+        // assert
+        $this->assertSame('1.00', $amountMatched);
     }
 }
