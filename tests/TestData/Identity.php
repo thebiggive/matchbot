e5d743ee591ef0e2363aa37d89b89c49f3e47ffd
Barney Laurance <barney@thebiggive.org.uk>
2024-01-12 14:28:43 +0000

    Refactor tests to compute JWT at runtime instead of using precomputed JWT
    Makes it easier to see and change what's going on here.

--- tests/TestData/Identity.php
+++ tests/TestData/Identity.php
@@ -4,11 +4,24 @@ declare(strict_types=1);
 
 namespace MatchBot\Tests\TestData;
 
+use Firebase\JWT\JWT;
+
 /**
  * Static Identity helpers for both unit & integration tests.
  */
 class Identity
 {
+    private const array INCOMPLETE_TOKEN_PAYLOAD = [
+        "iss" => "https://unit-test-fake-id-sub.thebiggivetest.org.uk",
+        "iat" => 1663436154,
+        "exp" => 2524608000,
+        "sub" => [
+            "person_id" => "12345678-1234-1234-1234-1234567890ab",
+            "complete" => false,
+            "psp_id" => "cus_aaaaaaaaaaaa11",
+        ]
+    ];
+
     public static function getTestPersonNewDonationEndpoint(): string
     {
         return '/v1/people/12345678-1234-1234-1234-1234567890ab/donations';
@@ -19,35 +32,30 @@ class Identity
      */
     public static function getTestIdentityTokenComplete(): string
     {
-        // As below but `"complete": true`.
-        return 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.' .
-            'eyJpc3MiOiJodHRwczovL3VuaXQtdGVzdC1mYWtlLWlkLXN1Yi50aGViaWdnaXZldGVzdC5vcmcudWsiLCJpYXQiOjE2NjM5NDQ4ODks' .
-            'ImV4cCI6MjUyNDYwODAwMCwic3ViIjp7InBlcnNvbl9pZCI6IjEyMzQ1Njc4LTEyMzQtMTIzNC0xMjM0LTEyMzQ1Njc4OTBhYiIsImNv' .
-            'bXBsZXRlIjp0cnVlLCJwc3BfaWQiOiJjdXNfYWFhYWFhYWFhYWFhMTEifX0.9zk7DUdvC9BWuRhXo2p7r12ZiTuREV7v9zsY97p_fyA';
+        $token = self::INCOMPLETE_TOKEN_PAYLOAD;
+        $token['sub']['complete'] = true;
+
+        return JWT::encode(
+            $token,
+            self::secret(),
+            'HS256',
+        );
     }
 
     public static function getTestIdentityTokenIncomplete(): string
     {
-        // One-time, artifically long token generated and hard-coded here so that we don't
-        // need live code just for MatchBot to issue ID tokens only for unit tests.
-        // Token is for Stripe Customer cus_aaaaaaaaaaaa11.
-        //
-        // Base 64 decoded body part:
-        // {
-        //  "iss":"https://unit-test-fake-id-sub.thebiggivetest.org.uk",
-        //  "iat":1663436154,
-        //  "exp":2524608000,
-        //  "sub": {
-        //      "person_id":"12345678-1234-1234-1234-1234567890ab",
-        //      "complete":false,
-        //      "psp_id":"cus_aaaaaaaaaaaa11"
-        //  }
-        // }
-        $dummyPersonAuthTokenValidUntil2050 = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3VuaXQtdGVz' .
-            'dC1mYWtlLWlkLXN1Yi50aGViaWdnaXZldGVzdC5vcmcudWsiLCJpYXQiOjE2NjM0MzYxNTQsImV4cCI6MjUyNDYwODAwMCwic3ViIjp7' .
-            'InBlcnNvbl9pZCI6IjEyMzQ1Njc4LTEyMzQtMTIzNC0xMjM0LTEyMzQ1Njc4OTBhYiIsImNvbXBsZXRlIjpmYWxzZSwicHNwX2lkIjoi' .
-            'Y3VzX2FhYWFhYWFhYWFhYTExIn19.KdeGTDkkWCjI4-Kayay0LKn9TXziPXCUxxTPIZgGxxE';
+        return JWT::encode(
+            self::INCOMPLETE_TOKEN_PAYLOAD,
+            self::secret(),
+            'HS256',
+        );
+    }
 
-        return $dummyPersonAuthTokenValidUntil2050;
+    public static function secret(): string
+    {
+        $secret = getenv('JWT_ID_SECRET');
+        \assert(is_string($secret));
+
+        return $secret;
     }
 }
