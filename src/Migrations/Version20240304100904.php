f1c61de7f2c21eb41a3115e01d2f9ae397bc1d5f
Barney Laurance <barney@thebiggive.org.uk>
2024-03-04 10:42:41 +0000

    CLA-37: Avoid possible duplicate GA claim for donations that charity is intending to claim for

---
+++ src/Migrations/Version20240304100904.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240304100904 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Avoid claiming gift aid for specific donations where charity is planning to claim it for themselves';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<SQL
+                        UPDATE Donation set Donation.tbgShouldProcessGiftAid = 0 
+                        WHERE Donation.campaign_id = 5483
+                        AND donationStatus != 'Paid'
+                        LIMIT 46
+                        SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(<<<SQL
+                        UPDATE Donation set Donation.tbgShouldProcessGiftAid = 1 
+                        WHERE Donation.campaign_id = 5483
+                        AND donationStatus != 'Paid'
+                        LIMIT 46
+                        SQL
+        );
+    }
+}
