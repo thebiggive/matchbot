cef1e98bbe0057e179fae21867ddc81211206992
Barney Laurance <barney@thebiggive.org.uk>
2024-03-14 14:34:56 +0000

    MAT-356: Refactor, add constructor to Donation HttpModel
    This will allow adding constraints to be enforced within the constuctor

--- src/Application/HttpModels/Donation.php
+++ src/Application/HttpModels/Donation.php
@@ -7,29 +7,35 @@ namespace MatchBot\Application\HttpModels;
 /**
  * Full Donation model for both request (webhooks) and response (create and get endpoints) use.
  */
-class Donation
+readonly class Donation
 {
-    public ?string $transactionId = null;
-    public string $status;
-    public string $charityId;
-    /** @var bool|null Used only to tell credit donations to complete; not persisted. */
-    public ?bool $autoConfirmFromCashBalance = false;
-    public ?string $currencyCode = null;
-    public float $donationAmount;
-    public ?float $feeCoverAmount = null;
-    public ?bool $giftAid;
-    public bool $donationMatched;
-    public ?string $firstName = null;
-    public ?string $lastName = null;
-    public ?string $emailAddress = null;
-    public ?string $billingPostalAddress = null;
-    public ?string $countryCode = null;
-    public ?string $homeAddress = null;
-    public ?string $homePostcode = null;
-    public ?bool $optInTbgEmail;
-    public ?bool $optInCharityEmail = null;
-    public ?bool $optInChampionEmail = null;
-    public string $projectId;
-    public ?float $tipAmount = null;
-    public ?bool $tipGiftAid = null;
+    /**
+     * @psalm-suppress PossiblyUnusedMethod - this constructor is called bye the Symfony Serializer
+     */
+    public function __construct(
+        public ?string $transactionId = null,
+        public ?string $status = null,
+        public ?string $charityId = null,
+        /** @var bool|null Used only to tell credit donations to complete; not persisted. */
+        public ?bool $autoConfirmFromCashBalance = null,
+        public ?string $currencyCode = null,
+        public float $donationAmount,
+        public ?float $feeCoverAmount = null,
+        public ?bool $giftAid,
+        public ?bool $donationMatched = null,
+        public ?string $firstName = null,
+        public ?string $lastName = null,
+        public ?string $emailAddress = null,
+        public ?string $billingPostalAddress = null,
+        public ?string $countryCode = null,
+        public ?string $homeAddress = null,
+        public ?string $homePostcode = null,
+        public ?bool $optInTbgEmail,
+        public ?bool $optInCharityEmail = null,
+        public ?bool $optInChampionEmail = null,
+        public ?string $projectId = null,
+        public ?float $tipAmount = null,
+        public ?bool $tipGiftAid = null,
+    ) {
+    }
 }
