0531dd6fb7981a4fd92220e4955f4fe534f81def
Barney Laurance <barney@thebiggive.org.uk>
2024-04-22 16:57:13 +0100

    MAT-358: Remove .build-commit-id file from repo

--- .build-commit-id.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Returns the commit ID of the git commit
-// used to build the current version. This is used as part of a cache namespace, and in future might also be shown
-// in a custom header or footer or something.
-
-throw new \Exception(
-    'Please ensure save-build-commit-id is run to get the commit id for use in non-local environments.'
-);
