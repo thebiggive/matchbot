9ce8eda09d0da09af44ee63303dcb2a1e9bddde7
Barney Laurance <barney@thebiggive.org.uk>
2024-05-02 12:06:01 +0100

    MAT-361: Increase fund amounts available to zero and reduce withdrawal sizes by matching delta

--- src/Migrations/Version20240502101845.php
+++ src/Migrations/Version20240502101845.php
@@ -20,17 +20,17 @@ final class Version20240502101845 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql(<<<SQL
-            -- reduce amount from 5_000 to 2_500, reduce amountAvailable from 1_890 to -610
-            UPDATE CampaignFunding SET amount = amount - 2500, amountAvailable = amountAvailable - 2500 WHERE id = 29101 LIMIT 1;
+            -- reduce amount from 5_000 to 2_500, reduce amountAvailable from 1_890 to 0
+            UPDATE CampaignFunding SET amount = amount - 2500, amountAvailable = amountAvailable - 1890 WHERE id = 29101 LIMIT 1;
 
-            -- reduce amount from 2_500 to zero;
-            UPDATE FundingWithdrawal set amount = amount - 2500 where id = 717813;
+            -- reduce amount from 2_500 to 1890;
+            UPDATE FundingWithdrawal set amount = amount - 610 where id = 717813;
             
-            -- reduce amount from 20_000 to 10_000, reduce amountAvailable from 9952 to -48
-            UPDATE CampaignFunding SET amount = amount - 10000, amountAvailable = amountAvailable - 10000 WHERE id = 29116 LIMIT 1;
+            -- reduce amount from 20_000 to 10_000, reduce amountAvailable from 9952 to 0
+            UPDATE CampaignFunding SET amount = amount - 10000, amountAvailable = amountAvailable - 9952 WHERE id = 29116 LIMIT 1;
 
             -- reduce amount from 10_000 to zero
-            UPDATE FundingWithdrawal set amount = amount - 10000 where id = 709467 LIMIT 1;
+            UPDATE FundingWithdrawal set amount = amount - 9952 where id = 709467 LIMIT 1;
             SQL);
     }
 
