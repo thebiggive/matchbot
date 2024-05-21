751cc7efa6f011b43a1b2c6bf1fbc3e91b3f76c9
Barney Laurance <barney@thebiggive.org.uk>
2024-05-21 17:01:46 +0100

    MAT-367: Resumbit gift aid claims

---
+++ src/Migrations/Version20240521155414.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240521155414 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Resubmit gift aid claims - see MAT-367';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $campaignIDForMAT367 = "4644";
+        $countForMAT367 = "97";
+
+        $this->addSql(<<<SQL
+            UPDATE Donation
+            SET
+                tbgGiftAidRequestQueuedAt = NULL,
+                tbgGiftAidRequestFailedAt = NULL,
+                tbgGiftAidRequestConfirmedCompleteAt = NULL,
+                tbgGiftAidRequestCorrelationId = NULL,
+                tbgGiftAidResponseDetail = NULL,
+                salesforcePushStatus = 'pending-update'
+            WHERE
+                campaign_id = $campaignIDForMAT367 AND
+                giftAid = 1 AND
+                tbgShouldProcessGiftAid = 1 AND
+                tbgGiftAidRequestCorrelationId IS NOT NULL AND
+                donationStatus = 'Paid'
+            LIMIT $countForMAT367;
+        SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new \Exception("No going back");
+    }
+}
