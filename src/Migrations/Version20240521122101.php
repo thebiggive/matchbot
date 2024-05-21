5d3623f74d45c87d8ab9d0c376e440876bba3e5c
Barney Laurance <barney@thebiggive.org.uk>
2024-05-21 13:24:56 +0100

    Remove GA for non-elgible donations
    (see slack thread in #gift-aid from May 2nd)

---
+++ src/Migrations/Version20240521122101.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240521122101 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Remove GA setting from inelligble donations';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<SQL
+            UPDATE Donation set giftAid = 0, salesforcePushStatus = 'pending-update'
+            WHERE Donation.salesforcePushStatus IN ('a0669000020MEiaAAG', 'a0669000020M9saAAC', 'a0669000020LtFrAAK')
+            LIMIT 3;
+            SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        throw new \Exception("no going back");
+    }
+}
