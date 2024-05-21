6eef6c32354359e11060b31460c372458f01a9a4
Barney Laurance <barney@thebiggive.org.uk>
2024-05-21 13:48:47 +0100

    Update src/Migrations/Version20240521122101.php
    Co-authored-by: Noel Light-Hilary <noel@biggive.org>

--- src/Migrations/Version20240521122101.php
+++ src/Migrations/Version20240521122101.php
@@ -17,7 +17,7 @@ final class Version20240521122101 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql(<<<SQL
-            UPDATE Donation set giftAid = 0, salesforcePushStatus = 'pending-update'
+            UPDATE Donation set giftAid = 0, tipGiftAid = 0, salesforcePushStatus = 'pending-update'
             WHERE Donation.salesforcePushStatus IN ('a0669000020MEiaAAG', 'a0669000020M9saAAC', 'a0669000020LtFrAAK')
             LIMIT 3;
             SQL
