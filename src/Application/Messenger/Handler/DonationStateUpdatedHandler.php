8b317b5e91a977130ae099478c1d8df6c7c8f6d9
Barney Laurance <barney@thebiggive.org.uk>
2024-06-19 16:42:29 +0100

    Revert "MAT-363: Add more detail to DSUH error logging"
    This reverts commit 1f514d206d72b7c56338330af9f4e22888f46947.

--- src/Application/Messenger/Handler/DonationStateUpdatedHandler.php
+++ src/Application/Messenger/Handler/DonationStateUpdatedHandler.php
@@ -72,12 +72,8 @@ class DonationStateUpdatedHandler implements BatchHandlerInterface
         try {
             $this->donationRepository->push($donation, $donationIsNew);
         } catch (\Throwable $exception) {
-            // getId() works on proxy object, does not trigger lazy loading
-            $campaginID = $donation->getCampaign()->getId();
             $this->logger->error(
-                "Exception on attempt to push donation $donationUUID, for campaign # $campaginID \n" .
-                "will nack $jobsForThisDonationCount jobs\n" .
-                $exception
+                "Exception on attempt to push donation, will nack $jobsForThisDonationCount jobs" . $exception
             );
             foreach ($jobsForThisDonation as $job) {
                 $job[1]->nack($exception);
