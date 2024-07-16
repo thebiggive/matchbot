dd76d4148da90468a2362ad628b6d9e567b02934
Barney Laurance <barney@thebiggive.org.uk>
2024-07-16 16:04:57 +0100

    MAT-368: Stop echoing queryies in data patch migration

--- src/Migrations/Version20240716142043.php
+++ src/Migrations/Version20240716142043.php
@@ -37,18 +37,13 @@ final class Version20240716142043 extends AbstractMigration
             $newFundSFID = $newFundDetails['0'];
             $newFundName = $newFundDetails['1'];
 
-            $query = <<<SQL
+            $this->addSql(<<<SQL
                 UPDATE Fund 
                 JOIN CampaignFunding on Fund.id = CampaignFunding.fund_id
                 JOIN Campaign_CampaignFunding on Campaign_CampaignFunding.campaignfunding_id = CampaignFunding.id
                 JOIN Campaign on Campaign.id = Campaign_CampaignFunding.campaign_id
                 SET Fund.salesforceId = '$newFundSFID', Fund.name = '$newFundName' WHERE Campaign.salesforceId = '$campaignSFID'
-            SQL;
-
-            echo $query;
-            echo "\n\n";
-
-            $this->addSql($query
+            SQL
             );
         }
 
