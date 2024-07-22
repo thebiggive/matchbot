0762122b46a2627740186c43033782cead2724af
Barney Laurance <barney@thebiggive.org.uk>
2024-07-22 10:41:30 +0100

    DON-983: Add DB index to allow efficiently looking up all donations for one donor

---
+++ src/Migrations/Version20240722094007.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240722094007 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        /**
+         * Donations will be looked up by this field for now to show them to customers, so needs index for performance:
+         */
+        return 'CREATE INDEX pspCustomerId ON Donation (pspCustomerId)';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE INDEX pspCustomerId ON Donation (pspCustomerId)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX pspCustomerId ON Donation');
+    }
+}
