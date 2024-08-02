7561f4cd097b5078112fb033eaf155da980287cf
Noel Light-Hilary <noel@noellh.com>
2024-08-02 10:23:35 +0100

    MAT-363 – improve logs when a past push seems to have failed

--- src/Domain/DonationRepository.php
+++ src/Domain/DonationRepository.php
@@ -642,9 +642,16 @@ class DonationRepository extends SalesforceWriteProxyRepository
 
         if ($proxiesToCreate !== []) {
             $count = count($proxiesToCreate);
-            $this->logger->error(
-                "Found $count pending items to push to SF, suggests push via Symfony Messenger failed"
+            // Warning for now. SF blips happen, especially in sandboxes. So we think this is bad
+            // enough to track on charts to see if volumes increase lots, but not to actively alert
+            // on as `.ERROR`.
+            $this->logger->warning(
+                "pushSalesforcePending found $count pending items to push to SF, suggests push via Symfony Messenger failed"
             );
+
+            $first3OrFewerProxies = array_slice($proxiesToCreate, 0, 3);
+            $firstUUIDs = array_map(static fn(Donation $d) => $d->getUuid(), $first3OrFewerProxies);
+            $this->logger->info('pushSalesforcePending sample UUIDs: ' . implode(', ', $firstUUIDs));
         }
 
         foreach ($proxiesToCreate as $proxy) {
