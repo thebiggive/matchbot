53bc163b135545b482ae6c1961523d6faaebab9a
Noel Light-Hilary <noel@noellh.com>
2024-08-13 16:42:13 +0100

    MAT-368 – bring back link to correct pledge

---
+++ src/Migrations/Version20240813153254.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * MAT-368 pt ii: Fix an overzealously patched pledge.
+ */
+final class Version20240813153254 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Move a pledge link back to the correct Fund';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql(<<<SQL
+            UPDATE CampaignFunding
+            SET fund_id = 24106
+            WHERE id = 29189 AND amount = 7500.00 AND allocationOrder = 100
+            LIMIT 1
+            SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // No un-patch.
+    }
+}
