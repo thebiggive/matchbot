4e8b1d1ebc6ae919a8b456629a48e4aa2b908561
Barney Laurance <barney@thebiggive.org.uk>
2024-10-04 11:39:08 +0100

    Fix Psalm MissingConstructor issue, simplify StripePayout class

--- src/Application/Messenger/StripePayout.php
+++ src/Application/Messenger/StripePayout.php
@@ -5,32 +5,12 @@ namespace MatchBot\Application\Messenger;
 /**
  * Model representing a Stripe Payout 'payout.paid' event for queued follow-up.
  */
-class StripePayout
+readonly class StripePayout
 {
-    public string $connectAccountId;
-    public string $payoutId;
-
-    public function getConnectAccountId(): string
+    public function __construct(
+        public string $connectAccountId,
+        public string $payoutId,
+    )
     {
-        return $this->connectAccountId;
-    }
-
-    public function setConnectAccountId(string $connectAccountId): self
-    {
-        $this->connectAccountId = $connectAccountId;
-
-        return $this;
-    }
-
-    public function getPayoutId(): string
-    {
-        return $this->payoutId;
-    }
-
-    public function setPayoutId(string $payoutId): self
-    {
-        $this->payoutId = $payoutId;
-
-        return $this;
     }
 }
