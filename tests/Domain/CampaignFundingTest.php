18e96a796e95c642a22fd74ea12b091ed1baccf2
Barney Laurance <barney@thebiggive.org.uk>
2024-10-07 12:23:26 +0100

    Increase test mutation coverage for CampaignFunding

---
+++ tests/Domain/CampaignFundingTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Domain;
+
+use MatchBot\Domain\CampaignFunding;
+use MatchBot\Domain\Pledge;
+use MatchBot\Tests\TestCase;
+
+class CampaignFundingTest extends TestCase {
+
+    public function testItAddsDistinctCampaignsOnly(): void
+    {
+        $campaignOne = self::someCampaign();
+        $campaignTwo = self::someCampaign();
+        // all constructor params are irrelevant here:
+        $campaignFunding = new CampaignFunding(new Pledge('GBP', 'some pledge'),'1','1', 1);
+
+        $campaignFunding->addCampaign($campaignOne);
+        $campaignFunding->addCampaign($campaignTwo);
+        $campaignFunding->addCampaign($campaignTwo);
+
+        $this->assertSame([$campaignOne, $campaignTwo], $campaignFunding->getCampaigns());
+    }
+}
