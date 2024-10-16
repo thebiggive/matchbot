3c8f91a24c64a22ce91e6be7071d493f23e0b229
Noel Light-Hilary <noel@noellh.com>
2024-10-16 18:27:16 +0100

    MAT-377 – give all messages unique IDs; log these with all sends and receives; log donation UUID alongside where applicable

---
+++ src/Application/Messenger/Stamp/MessageId.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Application\Messenger\Stamp;
+
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+use Symfony\Component\Messenger\Stamp\StampInterface;
+
+class MessageId implements StampInterface
+{
+    private UuidInterface $messageId;
+
+    public function __construct()
+    {
+        $this->messageId = Uuid::uuid4();
+    }
+
+    public function getMessageId(): string
+    {
+        return $this->messageId->toString();
+    }
+}
