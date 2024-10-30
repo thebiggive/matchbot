05eb5c996ef80e58ba1c9716529c11409eae4141
Barney Laurance <barney@thebiggive.org.uk>
2024-10-30 17:47:59 +0000

    Hotfix - temporarily stop running new matchbot:cancel-stale-donation-fund-tips

--- src/Application/Commands/CallFrequentTasks.php
+++ src/Application/Commands/CallFrequentTasks.php
@@ -25,7 +25,7 @@ class CallFrequentTasks extends LockingCommand
         $commandNames = [
             'matchbot:send-statistics',
             'matchbot:expire-match-funds',
-            'matchbot:cancel-stale-donation-fund-tips',
+//            'matchbot:cancel-stale-donation-fund-tips', <-- this new one is erroring in prod. Commenting out for now to fix in the morning
         ];
 
         $commands = array_map($app->find(...), $commandNames);
