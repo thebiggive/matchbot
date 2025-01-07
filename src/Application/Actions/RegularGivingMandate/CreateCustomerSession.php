6e3183e335016c8c22886cf445b8ceec049a5e5e
Barney Laurance <barney@thebiggive.org.uk>
2025-01-07 15:45:43 +0000

    DON-998: (9): Pull campaign from SF when donor creates stripe session
    Required to be able to set up a donation & mandate for the campaign.

--- src/Application/Actions/RegularGivingMandate/CreateCustomerSession.php
+++ src/Application/Actions/RegularGivingMandate/CreateCustomerSession.php
@@ -6,6 +6,8 @@ use Laminas\Diactoros\Response\JsonResponse;
 use MatchBot\Application\Actions\Action;
 use MatchBot\Application\Security\Security;
 use MatchBot\Client\Stripe;
+use MatchBot\Domain\CampaignRepository;
+use MatchBot\Domain\Salesforce18Id;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Log\LoggerInterface;
@@ -18,6 +20,7 @@ class CreateCustomerSession extends Action
     public function __construct(
         LoggerInterface $logger,
         private Stripe $stripeClient,
+        private CampaignRepository $campaignRepository,
         private Security $security,
     ) {
         parent::__construct($logger);
@@ -26,6 +29,14 @@ class CreateCustomerSession extends Action
     protected function action(Request $request, Response $response, array $args): Response
     {
         $donor = $this->security->requireAuthenticatedDonorAccountWithPassword($request);
+
+        $body = json_decode($request->getBody()->getContents(), associative: true);
+        \assert(is_array($body));
+        $campaignId = $body['campaignId'] ?? null;
+        if ($campaignId !== null) {
+            \assert(is_string($campaignId));
+            $this->campaignRepository->pullFromSFIfNotPresent(Salesforce18Id::ofCampaign($campaignId));
+        }
 
         $customerSession = $this->stripeClient->createRegularGivingCustomerSession($donor->stripeCustomerId);
 
