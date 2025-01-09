59de6847fe966273f04bb5977701420b74b22585
Barney Laurance <barney@thebiggive.org.uk>
2025-01-09 11:07:43 +0000

    DON-998: Refactor test, use shouldBeCalledOnce() instead of shouldBeCalledTimes(1)

--- integrationTests/CreateRegularGivingMandateTest.php
+++ integrationTests/CreateRegularGivingMandateTest.php
@@ -31,13 +31,13 @@ class CreateRegularGivingMandateTest extends IntegrationTest
         $stripeProphecy->createPaymentIntent(
             Argument::that(fn(array $payload) => ($payload['amount'] === $pencePerMonth))
         )
-            ->shouldBeCalledTimes(1)
+            ->shouldBeCalledOnce()
             ->will(fn() => new PaymentIntent('payment-intent-id-' . IntegrationTest::randomString()));
         $stripeProphecy->confirmPaymentIntent(
             Argument::type('string'),
             Argument::cetera()
         )
-            ->shouldBeCalledTimes(1)
+            ->shouldBeCalledOnce()
             ->will(fn(array $args) => new PaymentIntent($args[0]));
 
         $this->getContainer()->set(Stripe::class, $stripeProphecy->reveal());
