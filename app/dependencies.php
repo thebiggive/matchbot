54d0ff88131ce7d46379299df07dbebb263c9521
Noel Light-Hilary <noel@noellh.com>
2025-02-05 17:08:19 +0000

    BG2-2798 – add `FundTotalUpdated` message class to `RoutableMessageBus` container
    Just in case

--- app/dependencies.php
+++ app/dependencies.php
@@ -496,6 +496,7 @@ return function (ContainerBuilder $containerBuilder) {
             $busContainer->set(\Stripe\Event::PAYOUT_PAID, $bus);
             $busContainer->set(CharityUpdated::class, $bus);
             $busContainer->set(DonationUpserted::class, $bus);
+            $busContainer->set(FundTotalUpdated::class, $bus);
 
             return new RoutableMessageBus($busContainer, $bus);
         },
