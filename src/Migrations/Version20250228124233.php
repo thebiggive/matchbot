230843df04454173052556b3953703ac3ddc99e7
Barney Laurance <barney@thebiggive.org.uk>
2025-02-28 13:03:11 +0000

    DON-1005: Add DB constraint to stop one donor having two active mandates for same campaign

---
+++ src/Migrations/Version20250228124233.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250228124233 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        if (getenv('APP_ENV') !== 'production') {
+            // deleting mandates to avoid allow unique index to be created.
+            $this->addSql('DELETE from Donation WHERE mandate_id is not null');
+            $this->addSql('DELETE from RegularGivingMandate where id > 0');
+        }
+
+        // workaround for MySQL not supporting partial indexes. I don't think we can generate this via a Doctrine
+        // annotation. Based on similar index email_if_password used in identity service.
+        $this->addSql(sql: <<<'SQL'
+            CREATE UNIQUE INDEX person_id_if_active ON RegularGivingMandate(
+                (CASE WHEN RegularGivingMandate.status in ('active', 'pending') THEN concat(personid, ':', campaignId) END)
+            );
+        SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP INDEX person_id_if_active ON RegularGivingMandate');
+    }
+}
