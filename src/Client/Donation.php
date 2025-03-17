c8ca4c8742639816e0af54303947ab86c632126b
Barney Laurance <barney@thebiggive.org.uk>
2025-03-17 23:36:56 +0000

    MAT-400: Allow data in matchbot queue to be sent to SF despite mismatched keys

--- src/Client/Donation.php
+++ src/Client/Donation.php
@@ -23,9 +23,18 @@ class Donation extends Common
      */
     public function createOrUpdate(DonationUpserted $message): Salesforce18Id
     {
+        $jsonSnapshot = $message->jsonSnapshot;
+
+        // next lines should only be needed to fix some missing data in existing messages in queue that
+        // are not able to be pushed to salesforce as SF code is throwing an NPE missing this.
+        // Can be deleted immediately after deployment.
+        if (! array_key_exists('confirmationByMatchbot', $jsonSnapshot)) {
+            $jsonSnapshot['confirmationByMatchbot'] = false;
+        }
+
         return $this->postUpdateToSalesforce(
             $this->baseUri() . '/' . $message->uuid,
-            $message->jsonSnapshot,
+            $jsonSnapshot,
             $message->uuid,
             'donation',
         );
