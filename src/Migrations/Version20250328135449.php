868241207e9e70d13f2befad4c79e4d2458eb56d
Noel Light-Hilary <noel@noellh.com>
2025-03-28 13:57:23 +0000

    Get `Donation.tbgGiftAidRequestQueuedAt` Doctrine & PHP types back in sync

---
+++ src/Migrations/Version20250328135449.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatchBot\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Mark Donation.tbgGiftAidRequestQueuedAt as datetime_immutable
+ */
+final class Version20250328135449 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Mark Donation.tbgGiftAidRequestQueuedAt as datetime_immutable';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE Donation CHANGE tbgGiftAidRequestQueuedAt tbgGiftAidRequestQueuedAt DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE Donation CHANGE tbgGiftAidRequestQueuedAt tbgGiftAidRequestQueuedAt DATETIME DEFAULT NULL');
+    }
+}
