219ec1007106b7f75cc4421803d2b98789298257
Barney Laurance <barney@thebiggive.org.uk>
2025-04-17 14:58:52 +0100

    DON-1101: Reject long numbers in donor name

---
+++ tests/Domain/DonorNameTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Domain;
+
+use MatchBot\Application\LazyAssertionException;
+use MatchBot\Domain\DonorName;
+use MatchBot\Tests\TestCase;
+
+class DonorNameTest extends TestCase
+{
+    /** @dataProvider allowedAndNotAllowedNames */
+    public function testItAllowsSomeNamesAndNotOthers(string $firstName, string $lastName, bool $isAllowed): void
+    {
+        if (! $isAllowed) {
+            $this->expectException(LazyAssertionException::class);
+        }
+
+        $name = DonorName::of($firstName, $lastName);
+
+        if ($isAllowed) {
+            $this->assertSame("$firstName $lastName", $name->fullName());
+        }
+    }
+
+    /**
+     * @return array<array{0: string, 1: string, 2: boolean}>
+     */
+    public function allowedAndNotAllowedNames(): array
+    {
+        return [
+            'Typical name' => ['Joe', 'Bloggs', true],
+            'Very long name' => [str_repeat('f', 255), str_repeat('l', 255), true],
+            'Excessively long first name' => [str_repeat('f', 256), str_repeat('l', 255), false],
+            'Excessively long last name' => [str_repeat('f', 255), str_repeat('l', 256), false],
+            'Excessively short first name' => ['', 'Bloggs', false],
+            'Excessively short last name' => ['Joe', '', false],
+            'First name with long number (likeely entered by mistake)' => ['123 456', 'Bloggs', false],
+            'Last name with long number (likeely entered by mistake)' => ['Joe', '123 456', false],
+            'First Name with short number (maybe not a mistake)' => ['Joe the 12345th', 'Blogs ', true],
+        ];
+    }
+}
