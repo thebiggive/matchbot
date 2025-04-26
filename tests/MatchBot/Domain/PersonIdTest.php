a38b51ab2a1a558f6e8215858150fbf197b2ce4d
Barney Laurance <barney@thebiggive.org.uk>
2025-04-26 13:42:45 +0100

    Add tests for recently buggy PersonId

---
+++ tests/MatchBot/Domain/PersonIdTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MatchBot\Domain;
+
+use PHPUnit\Framework\TestCase;
+use Ramsey\Uuid\Uuid;
+
+class PersonIdTest extends TestCase {
+    public function testTwoIdsFromSameStringAreEqual(): void
+    {
+        $anyUUID = 'b13e4c9c-229a-11f0-bf58-1f1f1db0aefb';
+
+        self::assertTrue(PersonId::of($anyUUID)->equals(PersonId::of($anyUUID)));
+    }
+
+    public function testIsEqualToItself(): void
+    {
+        $personId = PersonId::of(Uuid::uuid4());
+        $itSelf = $personId;
+
+        self::assertTrue($personId->equals($itSelf));
+    }
+
+    public function testTwoIdsFromDifferentStringAreNotEqual(): void
+    {
+        $id1 = PersonId::of(Uuid::uuid4()->toString());
+        $id2 = PersonId::of(Uuid::uuid4()->toString());
+
+        self::assertFalse($id1->equals($id2));
+    }
+
+    public function testRoundTripFromUUID(): void
+    {
+        $uuid = Uuid::uuid4();
+        $this->assertTrue(PersonId::of($uuid)->id->equals($uuid));
+    }
+}
