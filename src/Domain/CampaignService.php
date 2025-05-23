6762eddf61ff75187c4bf1903ce35f0662653305
Barney Laurance <barney@thebiggive.org.uk>
2025-05-23 12:17:40 +0100

    MAT-411: Check campaign rendered from matchbot against what SF sent us.
    In future we will probably want to generate an alarm and/or throw if
    this doesn't match. Right now we know it doesn't match so we just
    include the list of errors in the response, and FE can display it on the
    donation page (at least outside prod) until we've fixed them all.

--- src/Domain/CampaignService.php
+++ src/Domain/CampaignService.php
@@ -2,6 +2,8 @@
 
 namespace MatchBot\Domain;
 
+use Assert\InvalidArgumentException;
+use Assert\LazyAssertionException;
 use MatchBot\Application\Assertion;
 use MatchBot\Application\HttpModels\Campaign as CampaignHttpModel;
 use MatchBot\Domain\Campaign as CampaignDomainModel;
@@ -15,8 +17,10 @@ class CampaignService
      * we may need to call out to SF to get the most current data as required for some fields, e.g. amountRaised,
      * although if SF is not available or doesn't know this campaign we could fall back to a null or default value
      * perhaps.
+     *
+     * @return array<string, mixed> Campaign as associative array ready to render as JSON and send to FE
      */
-    public function renderCampaign(CampaignDomainModel $campaign): CampaignHttpModel
+    public function renderCampaign(CampaignDomainModel $campaign): array
     {
         $charity = $campaign->getCharity();
 
@@ -45,7 +49,7 @@ class CampaignService
         Assertion::inArray($campaignStatus, ['Active','Expired','Preview']);
         /** @var 'Active'|'Expired'|'Preview' $campaignStatus */
 
-        return new CampaignHttpModel(
+        $campaignHttpModel = new CampaignHttpModel(
             id: $campaign->getSalesforceId(),
             amountRaised: 0, // MAT-411-todo-before merge - replace this and similar placeholder values
             additionalImageUris: [],
@@ -94,5 +98,34 @@ class CampaignService
             usesSharedFunds: false,
             video: null,
         );
+
+        /** @var array<string, mixed> $campaignHttpModelArray */
+        $campaignHttpModelArray = \json_decode(
+            json: \json_encode($campaignHttpModel, \JSON_THROW_ON_ERROR),
+            associative: true,
+            depth: 512,
+            flags: JSON_THROW_ON_ERROR
+        );
+
+        // We could just return $modelGeneratedFromSF to FE and not need to generate anything else with matchbot
+        // logic, but that would keep FE indirectly coupled to the SF service. By making sure matchbot is able to
+        // semi-independently regenerate the same thing we should be able to break the dependency and then later evolve
+        // the mathbot<->frontend interface without needing to change SF.
+        $modelGeneratedFromSF = $campaign->getSalesforceData() + ['charity' => $campaign->getCharity()->getSalesforceData()];
+
+        try {
+            CampaignRenderCompatibilityChecker::checkCampaignHttpModelMatchesModelFromSF($campaignHttpModelArray, $modelGeneratedFromSF);
+        } catch (LazyAssertionException $exception) {
+            $errorMessages = \array_map(
+                fn(InvalidArgumentException $e) => ["{$e->getPropertyPath()}: {$e->getMessage()}"],
+                $exception->getErrorExceptions()
+            );
+
+            \ksort($errorMessages);
+
+            $campaignHttpModelArray['errors'] = $errorMessages;
+        }
+
+        return $campaignHttpModelArray;
     }
 }
