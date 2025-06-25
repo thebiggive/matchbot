ce1e302513612cd34e4ff8bcf247557c0d44a91e
Barney Laurance <barney@thebiggive.org.uk>
2025-06-25 10:49:27 +0100

    MAT-413: Molify Psalm

--- src/Application/Actions/Campaigns/Search.php
+++ src/Application/Actions/Campaigns/Search.php
@@ -85,11 +85,12 @@ class Search extends Action
             term: $term,
         );
 
-        // TODO performant summaries – most notably `amountRaised` and `matchFundsRemaining` should
-        // come from future stats table.
-        // Some campaigns have SF data {} when they were last synced before we saved full SF data. If we try
-        // to render those there are missing array keys for beneficiaries et al.
-        // @psalm-suppress RedundantConditionGivenDocblockType We'll soon load all campaign SF data.
+        /**  TODO performant summaries – most notably `amountRaised` and `matchFundsRemaining` should
+         * come from future stats table.
+         * Some campaigns have SF data {} when they were last synced before we saved full SF data. If we try
+         * to render those there are missing array keys for beneficiaries et al.
+         * @psalm-suppress RedundantConditionGivenDocblockType We'll soon load all campaign SF data. 
+         * */
         $campaignsWithSfData = array_filter($campaigns, fn($campaign) => $campaign->getSalesforceData() !== []);
         $campaignSummaries = \array_map($this->campaignService->renderCampaignSummary(...), $campaignsWithSfData);
 
