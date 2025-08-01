35d57cf64aab4b27a359b057f7c01f935ebeb4c8
Noel Light-Hilary <noel@noellh.com>
2025-08-01 10:06:58 +0100

    Permit metacampaign slugs of lengths allowed in SF (except 1 character)

--- src/Domain/MetaCampaignSlug.php
+++ src/Domain/MetaCampaignSlug.php
@@ -13,7 +13,7 @@ readonly class MetaCampaignSlug
     private function __construct(
         public string $slug,
     ) {
-        Assertion::betweenLength($slug, minLength: 5, maxLength: 50);
+        Assertion::betweenLength($slug, minLength: 2, maxLength: 100);
         Assertion::regex($slug, '/^[A-Za-z0-9-]+$/');
     }
 
