9812b19e53d9c05a990b0697b021432bbef1fc69
Barney Laurance <barney@thebiggive.org.uk>
2025-08-11 18:03:14 +0100

    BG2-2917: Fix docs generation

---
+++ src/OpenApi.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MatchBot;
+
+/**
+ * @OA\Info(
+ *     title="MatchBot API",
+ *     version="1.0.0",
+ *     description="Microservice providing donation matching and related APIs",
+ *     @OA\Contact(
+ *         name="The Big Give",
+ *         url="https://www.thebiggive.org.uk",
+ *         email="tech@thebiggive.org.uk"
+ *     ),
+ *     @OA\License(
+ *         name="MIT",
+ *         url="https://github.com/thebiggive/matchbot/blob/main/LICENSE"
+ *     )
+ * )
+ * 
+ * @OA\Server(
+ *     url="https://matchbot.thebiggive.org.uk",
+ *     description="Production server"
+ * )
+ * 
+ * @OA\Server(
+ *     url="https://matchbot-staging.thebiggive.org.uk",
+ *     description="Staging server"
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     securityScheme="bearerAuth",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="JWT"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="donations",
+ *     description="Donation operations"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="campaigns",
+ *     description="Campaign operations"
+ * )
+ * 
+ * @OA\Tag(
+ *     name="charities",
+ *     description="Charity operations"
+ * )
+ * 
+ * @OA\PathItem(
+ *     path="/placeholder"
+ * )
+ */
+class OpenApi
+{
+    // This class exists solely to hold OpenAPI annotations
+}
