c39327eb2a4e145453d9f4144c95826fd67485ad
Barney Laurance <barney@thebiggive.org.uk>
2025-08-13 15:25:24 +0100

    BG2-2917: Convert more OpenAPI docs to attribute, remove spurious link to MIT license we don't have

--- src/OpenApi.php
+++ src/OpenApi.php
@@ -2,61 +2,48 @@
 
 namespace MatchBot;
 
+use OpenApi\Attributes as OA;
+
 /**
  * @psalm-suppress UnusedClass
- *
- * @OA\Info(
- *     title="MatchBot API",
- *     version="1.0.0",
- *     description="Microservice providing donation matching and related APIs",
- *     @OA\Contact(
- *         name="The Big Give",
- *         url="https://www.thebiggive.org.uk",
- *         email="tech@thebiggive.org.uk"
- *     ),
- *     @OA\License(
- *         name="MIT",
- *         url="https://github.com/thebiggive/matchbot/blob/main/LICENSE"
- *     )
- * )
- *
- * @OA\Server(
- *     url="https://matchbot.thebiggive.org.uk",
- *     description="Production server"
- * )
- *
- * @OA\Server(
- *     url="https://matchbot-staging.thebiggive.org.uk",
- *     description="Staging server"
- * )
- *
- * @OA\SecurityScheme(
- *     securityScheme="bearerAuth",
- *     type="http",
- *     scheme="bearer",
- *     bearerFormat="JWT"
- * )
- *
- * @OA\Tag(
- *     name="donations",
- *     description="Donation operations"
- * )
- *
- * @OA\Tag(
- *     name="campaigns",
- *     description="Campaign operations"
- * )
- *
- * @OA\Tag(
- *     name="charities",
- *     description="Charity operations"
- * )
- *
- * @OA\PathItem(
- *     path="/placeholder"
- * )
  */
+#[OA\Info(
+    title: "MatchBot API",
+    version: "1.0.0",
+    description: "Microservice providing donation matching and related APIs",
+    contact: new OA\Contact(
+        name: "The Big Give",
+        url: "https://www.thebiggive.org.uk",
+        email: "tech@thebiggive.org.uk"
+    ),
+)]
+#[OA\Server(
+    url: "https://matchbot.thebiggive.org.uk",
+    description: "Production server"
+)]
+#[OA\Server(
+    url: "https://matchbot-staging.thebiggive.org.uk",
+    description: "Staging server"
+)]
+#[OA\SecurityScheme(
+    securityScheme: "bearerAuth",
+    type: "http",
+    scheme: "bearer",
+    bearerFormat: "JWT"
+)]
+#[OA\Tag(
+    name: "Core donation journey"
+)]
+#[OA\Tag(
+    name: "Regular giving"
+)]
+#[OA\Tag(
+    name: "Charities"
+)]
+#[OA\PathItem(
+    path: "/placeholder"
+)]
 class OpenApi
 {
-    // This class exists solely to hold OpenAPI annotations
+    // This class exists solely to hold OpenAPI attributes
 }
