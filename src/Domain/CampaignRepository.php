00437e964e9f2359b21b17f2515d5a33af257f4d
Barney Laurance <barney@thebiggive.org.uk>
2025-08-22 15:27:11 +0100

    Merge pull request #1589 from thebiggive/BG2-2907-update-SCW2025-campaigns-from-sf
    BG2-2907: Update SCW 2025 campaigns from SF

--- src/Domain/CampaignRepository.php
+++ src/Domain/CampaignRepository.php
@@ -58,6 +58,7 @@ class CampaignRepository extends SalesforceReadProxyRepository
                 charity.tbgApprovedToClaimGiftAid = 0 AND
                 c.endDate >= :extendedLookbackDate
             )
+            OR c.metaCampaignSlug = 'small-charity-week-2025' -- to cover changes to funding allocations
             OR c.isRegularGiving = 1
             ORDER BY c.createdAt ASC
             DQL
