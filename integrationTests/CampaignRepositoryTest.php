0136e4aa322e8be1ed6edca0e290a8f54c25c049
Barney Laurance <barney@thebiggive.org.uk>
2026-01-20 14:26:14 +0000

    MAT-470: Reverse order of sort by relavence - testing on staging suggests it's wrong.

--- integrationTests/CampaignRepositoryTest.php
+++ integrationTests/CampaignRepositoryTest.php
@@ -422,9 +422,9 @@ class CampaignRepositoryTest extends IntegrationTest
                     ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ],
                 [
-                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
-                    ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
                     ['Charity Name', 'Campaign Two is for Porridge and Juice'],
+                    ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
+                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ],
             ],
             [
@@ -440,18 +440,18 @@ class CampaignRepositoryTest extends IntegrationTest
                     ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ],
                 [
-                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
-                    ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
                     ['Charity Name', 'Campaign Two is for Porridge and Juice'],
+                    ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
+                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ]
             ],
             [
                 'Freds Charity',
                 [],
                 [
-                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
-                    ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
                     ['Charity Name', 'Campaign Two is for Porridge and Juice'],
+                    ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
+                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ]
             ],
             [
@@ -461,8 +461,8 @@ class CampaignRepositoryTest extends IntegrationTest
                     ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ],
                 [
-                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                     ['Fred\'s Charity', 'This is a campaign for Fred\'s Charity'],
+                    ['Fred\'s Charity', 'This is a campaign name that does not mention the charity name'],
                 ]
             ],
             [
