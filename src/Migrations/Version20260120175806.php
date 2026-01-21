5a12578f520eb96c27e822b0911d5401699117c2
Barney Laurance <barney@thebiggive.org.uk>
2026-01-21 11:43:35 +0000

    Update src/Migrations/Version20260120175806.php

--- src/Migrations/Version20260120175806.php
+++ src/Migrations/Version20260120175806.php
@@ -7,14 +7,11 @@ namespace MatchBot\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20260120175806 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add full text indexes for Charity and Campaign name';
     }
 
     public function up(Schema $schema): void
