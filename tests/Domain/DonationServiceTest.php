152586f4b731846df343d470e6e06f179674ba66
Barney Laurance <barney@thebiggive.org.uk>
2026-03-11 14:54:17 +0000

    MAT-248: WIP update DonationServiceTest to work with Doctrine ORM 3

--- tests/Domain/DonationServiceTest.php
+++ tests/Domain/DonationServiceTest.php
@@ -102,6 +102,10 @@ class DonationServiceTest extends TestCase
         $donationCreate = $this->getDonationCreate();
         $donation = $this->getDonation();
         $campaignRepoProphecy->findOneBy(['salesforceId' => self::CAMPAIGN_ID])->willReturn($donation->getCampaign());
+        $this->entityManagerProphecy->beginTransaction()->shouldBeCalled();
+        $this->entityManagerProphecy->commit()->shouldBeCalled();
+        $this->entityManagerProphecy->flush()->shouldBeCalled();
+
 
         $this->chatterProphecy->send(
             Argument::type(ChatMessage::class),
@@ -138,6 +142,7 @@ class DonationServiceTest extends TestCase
         $campaignRepoProphecy->findOneBy(['salesforceId' => self::CAMPAIGN_ID])
             ->willReturn($donation->getCampaign());
 
+        $this->entityManagerProphecy->beginTransaction()->shouldBeCalled();
         $this->stripeProphecy->createPaymentIntent(Argument::any())
             ->willReturn($this->prophesize(\Stripe\PaymentIntent::class)->reveal());
 
@@ -218,11 +223,7 @@ class DonationServiceTest extends TestCase
             );
         } else {
             $this->entityManagerProphecy->persist(Argument::type(Donation::class))->willReturn(null);
-            $this->entityManagerProphecy->flush()->shouldBeCalled();
         }
-
-        $this->entityManagerProphecy->beginTransaction()->shouldBeCalled();
-        $this->entityManagerProphecy->commit()->shouldBeCalled();
 
         $logger = $logger ?? new NullLogger();
 
@@ -363,6 +364,8 @@ class DonationServiceTest extends TestCase
         $donation = TestCase::someDonation(amount: '15.00');
         $donation->setTransactionId($paymentIntentId);
 
+        $this->entityManagerProphecy->flush()->shouldBeCalled();
+
         $this->stripeProphecy->retrieveConfirmationToken($confirmationTokenId)
             ->will(function () {
                 $confirmationToken = new ConfirmationToken();
@@ -465,6 +468,7 @@ class DonationServiceTest extends TestCase
             StripeCustomerId::of('cus_someCustomerID'),
             StripePaymentMethodId::of('pm_paymentmethodid')
         )->willReturn($currentPaymentMethod);
+        $this->entityManagerProphecy->flush()->shouldBeCalled();
 
         $this->sut->confirmOnSessionDonation($donation, $stripeConfirmationTokenId, null);
 
