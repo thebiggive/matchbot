e19356df48de30b54dc864a38d6875db7c0b2267
Barney Laurance <barney@thebiggive.org.uk>
2026-05-28 17:08:52 +0100

    Update src/Domain/FundRepository.php

--- src/Domain/FundRepository.php
+++ src/Domain/FundRepository.php
@@ -131,7 +131,7 @@ class FundRepository extends SalesforceReadProxyRepository
     }
 
     /**
-     * @param array{currencyCode: ?string, name: ?string, slug: ?string, type: string, id:string, ...<mixed>} $fundData
+     * @param array{currencyCode: ?string, name: ?string, slug: ?string, type: string, id:string, ...} $fundData
      */
     private function setAnyFundData(Fund $fund, array $fundData): Fund
     {
