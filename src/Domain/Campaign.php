a22fa529ab5a76b3f0897a6bd265a62f9763bfd3
Barney Laurance <barney@thebiggive.org.uk>
2026-06-04 12:01:29 +0100

    BG2-3065: Remove unecassary null coalesce, expect some issues for mago

--- src/Domain/Campaign.php
+++ src/Domain/Campaign.php
@@ -79,6 +79,7 @@ class Campaign extends SalesforceReadProxy
     #[ORM\Column(type: 'string')]
     protected string $name;
 
+    // @mago-expect analysis:write-only-property
     #[ORM\Column(nullable: true, length: 5_000)]
     private ?string $summary;
 
@@ -168,6 +169,7 @@ class Campaign extends SalesforceReadProxy
     #[ORM\Embedded(columnPrefix: 'total_fundraising_target_')]
     private(set) Money $totalFundraisingTarget;
 
+    // @mago-expect analysis:write-only-property
     /**
      * Optional BG-defined default sort override for the metacampaign grid. Works as a rank value when set,
      * typically positive but not *required* to be positive or unique.
@@ -180,6 +182,7 @@ class Campaign extends SalesforceReadProxy
     #[ORM\Column(nullable: true)]
     private ?int $pinPosition;
 
+    // @mago-expect analysis:write-only-property
     /**
      * Optional BG-defined default sort override specifically for the funder-filtered view of a metacampaign.
      *
@@ -261,7 +264,7 @@ class Campaign extends SalesforceReadProxy
         $regularGivingCollectionObject = $regularGivingCollectionEnd === null ?
             null : new \DateTimeImmutable($regularGivingCollectionEnd);
 
-        $isPublished = $campaignData['isPublished'] ?? false;
+        $isPublished = $campaignData['isPublished'];
 
         $startDate = $campaignData['startDate'];
         $endDate = $campaignData['endDate'];
@@ -362,6 +365,7 @@ class Campaign extends SalesforceReadProxy
             // envrionments
 
             $_charity = $this->charity;
+            // @mago-expect analysis:avoid-catching-error
         } catch (\Error $e) {
             throw new \Exception(
                 "Error on attempt to persist campaign #{$this->id}, sfID {$this->getSalesforceId()}: \n{$e}"
@@ -629,6 +633,7 @@ class Campaign extends SalesforceReadProxy
      */
     public function getSalesforceData(): array
     {
+        // @mago-expect analysis:invalid-return-statement
         return $this->salesforceData + ['charity' => $this->charity->getSalesforceData()]; // @phpstan-ignore return.type
     }
 
