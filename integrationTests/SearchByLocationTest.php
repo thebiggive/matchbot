379ae070f10f29b3f540ac65bb67d3567d48da25
Barney Laurance <barney@thebiggive.org.uk>
2026-06-22 15:12:04 +0100

    DON-1205: Add campaign with other impact location to test, clarify tests

--- integrationTests/SearchByLocationTest.php
+++ integrationTests/SearchByLocationTest.php
@@ -17,6 +17,15 @@ use MatchBot\Tests\TestCase;
  */
 class SearchByLocationTest extends IntegrationTest
 {
+    private const string CAMPAIGN_FOR_CAMDEN_NAME = 'Campaign For Camden';
+    private const string CAMPAIGN_FOR_HARINGEY_NAME = 'Campaign For Haringey';
+    private const string CAMPAIGN_FOR_LONDON_NAME = 'Campaign For London';
+    private const string CAMPAIGN_FOR_UK_NAME = 'Campaign For UK';
+    private const string REGION_CODE_HARINGEY = 'E09000014';
+    private const string REGION_CODE_CAMDEN = 'E09000007';
+    private const string REGION_CODE_LONDON = 'E12000007';
+    private const string REGION_CODE_ENGLAND = 'E92000001';
+
     public function testSearchSortsByLocation(): void
     {
         $sut = $this->getService(CampaignRepository::class);
@@ -28,7 +37,7 @@ class SearchByLocationTest extends IntegrationTest
 
         $UKCampaign = $this->createCampaign(
             charity: $charity,
-            name: 'Campaign For UK',
+            name: self::CAMPAIGN_FOR_UK_NAME,
             status: CampaignStatus::Active,
             withUniqueSalesforceId: true,
             metaCampaignSlug: $slug,
@@ -46,7 +55,7 @@ class SearchByLocationTest extends IntegrationTest
 
         $londonCampaign = $this->createCampaign(
             charity: $charity,
-            name: 'Campaign For London',
+            name: self::CAMPAIGN_FOR_LONDON_NAME,
             status: CampaignStatus::Active,
             withUniqueSalesforceId: true,
             metaCampaignSlug: $slug,
@@ -56,7 +65,7 @@ class SearchByLocationTest extends IntegrationTest
 
         $londonCampaign->replaceLocations([
             ['countryName' => 'uk', 'regionCode' => null],
-            ['countryName' => null, 'regionCode' => 'E12000007'],
+            ['countryName' => null, 'regionCode' => self::REGION_CODE_LONDON],
         ]);
         $stats = CampaignStatistics::zeroPlaceholder($londonCampaign, new \DateTimeImmutable('now'));
         $em->persist($stats);
@@ -65,7 +74,7 @@ class SearchByLocationTest extends IntegrationTest
 
         $haringeyCampaign = $this->createCampaign(
             charity: $charity,
-            name: 'Campaign For Haringey',
+            name: self::CAMPAIGN_FOR_HARINGEY_NAME,
             status: CampaignStatus::Active,
             withUniqueSalesforceId: true,
             metaCampaignSlug: $slug,
@@ -75,11 +84,31 @@ class SearchByLocationTest extends IntegrationTest
 
         $haringeyCampaign->replaceLocations([
             ['countryName' => 'uk', 'regionCode' => null],
-            ['countryName' => null, 'regionCode' => 'E09000014'],
+            ['countryName' => null, 'regionCode' => self::REGION_CODE_HARINGEY],
         ]);
         $stats = CampaignStatistics::zeroPlaceholder($haringeyCampaign, new \DateTimeImmutable('now'));
 
         $em->persist($haringeyCampaign);
+
+        $em->persist($stats);
+
+        $camdenCampaign = $this->createCampaign(
+            charity: $charity,
+            name: self::CAMPAIGN_FOR_CAMDEN_NAME,
+            status: CampaignStatus::Active,
+            withUniqueSalesforceId: true,
+            metaCampaignSlug: $slug,
+            relatedApplicationStatus: ApplicationStatus::Approved,
+            relatedApplicationCharityResponseToOffer: CharityResponseToOffer::Accepted
+        );
+
+        $camdenCampaign->replaceLocations([
+            ['countryName' => 'uk', 'regionCode' => null],
+            ['countryName' => null, 'regionCode' => self::REGION_CODE_CAMDEN],
+        ]);
+        $stats = CampaignStatistics::zeroPlaceholder($camdenCampaign, new \DateTimeImmutable('now'));
+
+        $em->persist($camdenCampaign);
 
         $em->persist($stats);
 
@@ -88,7 +117,7 @@ class SearchByLocationTest extends IntegrationTest
         $returnValue = $sut->search(
             sortField: 'location',
             sortDirection: 'desc',
-            regions: ['E09000014','E12000007', 'E92000001'],
+            regions: [self::REGION_CODE_HARINGEY, self::REGION_CODE_LONDON, self::REGION_CODE_ENGLAND],
             offset: 0,
             limit: 6,
             metaCampaignSlug: $slug,
@@ -102,6 +131,11 @@ class SearchByLocationTest extends IntegrationTest
             $returnValue
         );
 
-        $this->assertSame(['Campaign For Haringey', 'Campaign For London', 'Campaign For UK'], $returnCampaignNames);
+        $this->assertSame([
+            self::CAMPAIGN_FOR_HARINGEY_NAME,
+            self::CAMPAIGN_FOR_LONDON_NAME,
+            self::CAMPAIGN_FOR_UK_NAME,
+            self::CAMPAIGN_FOR_CAMDEN_NAME // camden doesn't match any of the user's search regions, so would rank equal to UK campaign, then ranks lower because it has a higher auto increment ID.
+        ], $returnCampaignNames);
     }
 }
