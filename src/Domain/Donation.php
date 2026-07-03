bbf96705c2ab1bd841d9c64429ee3f9c294078c0
Barney Laurance <barney@thebiggive.org.uk>
2026-07-03 10:36:00 +0100

    MAT-488: Adjust docblock
    Co-authored-by: Noel Light-Hilary <noel@biggive.org>

--- src/Domain/Donation.php
+++ src/Domain/Donation.php
@@ -140,8 +140,8 @@ class Donation extends SalesforceWriteProxy
 
     /**
      * Time until which match any reserved match funds should be kept, *even*
-     * if the donation client goes offline. If they are online they can be expected to ask
-     * to extend this periodically, and matchbot will comply up to a limit of about half an hour.
+     * if the donation client goes offline. If they are online and campaign has time left they can be expected to ask
+     * to extend this periodically, and matchbot will comply up to a limit of about 25 minutes.
      *
      * Initially only set if requested by the client at creation time, so that we know the client
      * is a version that knows how to request extensions.
