62563e9a95a27cb570df96b6f74275187b56344f
Barney Laurance <barney@thebiggive.org.uk>
2026-07-10 18:06:02 +0100

    MAT-493: Molify static analysis in Base64PhpSerializer

--- src/Application/Messenger/Base64PhpSerializer.php
+++ src/Application/Messenger/Base64PhpSerializer.php
@@ -9,16 +9,23 @@ use Symfony\Component\Messenger\Transport\Serialization\PhpSerializer;
 
 class Base64PhpSerializer extends PhpSerializer
 {
+    /**
+     * @param array<array-key, mixed> $encodedEnvelope
+     */
+    #[\Override]
     public function decode(array $encodedEnvelope): Envelope
     {
-        if (empty($encodedEnvelope['body'])) {
-            throw new \Symfony\Component\Messenger\Exception\MessageDecodingFailedException('Encoded envelope should have at least a "body".');
+        if (empty($encodedEnvelope['body']) || !is_string($encodedEnvelope['body'])) {
+            throw new \Symfony\Component\Messenger\Exception\MessageDecodingFailedException('Encoded envelope should have at least a "body" string.');
         }
 
         $body = $encodedEnvelope['body'];
 
         if (!str_contains($body, '{') && !str_contains($body, ':')) {
-            $body = base64_decode($body, true) ?: $body;
+            $decodedBody = base64_decode($body, true);
+            if (is_string($decodedBody)) {
+                $body = $decodedBody;
+            }
         }
 
         // Symfony's PhpSerializer::decode does stripslashes() before unserializing.
