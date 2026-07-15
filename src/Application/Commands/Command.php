db499c89883555a0b0fba0fb53a77281af693260
Barney Laurance <barney@thebiggive.org.uk>
2026-07-15 10:32:45 +0100

    MAT-493: Refactor, remove duplicate list of command classes

--- src/Application/Commands/Command.php
+++ src/Application/Commands/Command.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace MatchBot\Application\Commands;
 
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Psr\Http\Message\StreamInterface;
+use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\Command\ConsumeMessagesCommand;
 
 abstract class Command extends SymfonyCommand
 {
@@ -48,5 +47,45 @@ abstract class Command extends SymfonyCommand
     private function noLog(InputInterface $input): bool
     {
         return $input->hasOption(self::CLI_OPTION_NOLOG) && $input->getOption(self::CLI_OPTION_NOLOG);
+    }
+
+    /**
+     * Returns all commands available in the application. We could consider changing the implementation to
+     * something that scans the directory.
+     *
+     * @return list<\Symfony\Component\Console\Command\Command>
+     */
+    public static function allCommands(ContainerInterface $c): array
+    {
+        $commands = array_map($c->get(...), [
+            // Alphabetical list:
+            CallFrequentTasks::class,
+            CancelStaleDonationFundTips::class,
+            ClaimGiftAid::class,
+            ConsumeMessagesCommand::class,
+            CreateFictionalData::class,
+            DeleteOldTestFunds::class,
+            ExpireMatchFunds::class,
+            ExpirePendingMandates::class,
+            HandleOutOfSyncFunds::class,
+            MergeOpenApiDocs::class,
+            PullIndividualCampaignFromSF::class,
+            PullMetaCampaignFromSF::class,
+            PushDailyFundTotals::class,
+            PushDonations::class,
+            RedistributeMatchFunds::class,
+            ResetMatching::class,
+            RetrospectivelyMatch::class,
+            ScheduledOutOfSyncFundsCheck::class,
+            SendStatistics::class,
+            SetupTestMandate::class,
+            TakeRegularGivingDonations::class,
+            UpdateApproxCampaignStatus::class,
+            UpdateCampaignDonationStats::class,
+            UpdateCampaigns::class,
+            WriteSchemaFile::class,
+        ]);
+
+        return $commands; // @mago-expect analysis:less-specific-nested-return-statement -- psalm knows the type is right, apparently Mago doesn't.
     }
 }
