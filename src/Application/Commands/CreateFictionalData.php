27a17875fa28493807c5271d91ec03470929619d
Barney Laurance <barney@thebiggive.org.uk>
2026-09-10 15:21:14 +0100

    BG2-3354: Add more regions to fictional campaigns for testing

--- src/Application/Commands/CreateFictionalData.php
+++ src/Application/Commands/CreateFictionalData.php
@@ -279,8 +279,11 @@ class CreateFictionalData extends Command
 
         if ($regionRandomizer < 10) {
             $regions[] = ['countryName' => null, 'regionCode' => 'E09000033']; // City of Westminister
+            $regions[] = ['countryName' => null, 'regionCode' => 'E12000007']; // London
         } elseif ($regionRandomizer < 25) {
             $regions[] = ['countryName' => null, 'regionCode' => 'E09000014']; // Haringey
+            $regions[] = ['countryName' => null, 'regionCode' => 'E12000007']; // London
+            $regions[] = ['countryName' => null, 'regionCode' => 'E12000006']; // East of England (one campaign can work in two places)
         } elseif ($regionRandomizer < 50) {
             $regions[] = ['countryName' => null, 'regionCode' => 'E12000007']; // London
         }
